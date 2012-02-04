bdc95beb077f00f61e6bc96e73cb3a4a6cff75bc
Alexander Leader <alexander.leader@infinum.com>
2012-02-04 16:43:48 +0000

    Mocked Challenge Lookup Service.
    git-svn-id: https://studentzen.svn.cvsdude.com/sspopensource/SSP/trunk@10 14c17786-f9a5-455e-acda-2e4726e2ef49

---
+++ src/main/java/edu/sinclair/ssp/model/transferobject/ChallengeTO.java
@@ -0,0 +1,36 @@
+package edu.sinclair.ssp.model.transferobject;
+
+public class ChallengeTO {
+
+	private String id;
+	private String name;
+	private String description;
+	
+	public ChallengeTO() {}
+	
+	public ChallengeTO(String id, String name, String description) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
+
---
+++ src/main/java/edu/sinclair/ssp/web/ChallengeController.java
@@ -0,0 +1,46 @@
+package edu.sinclair.ssp.web;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.sinclair.ssp.model.transferobject.ChallengeTO;
+
+@Controller
+@RequestMapping("/api/challenge")
+public class ChallengeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(ChallengeController.class);
+
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public @ResponseBody List<ChallengeTO> getAll() throws Exception {
+
+		List<ChallengeTO> challengeTOs = new ArrayList<ChallengeTO>();
+		
+		challengeTOs.add(new ChallengeTO(UUID.randomUUID().toString(), "Transportation", "Transportation is a challenge for many."));
+		challengeTOs.add(new ChallengeTO(UUID.randomUUID().toString(), "Finances", "Financial aid is a challenge for most college students."));
+		challengeTOs.add(new ChallengeTO(UUID.randomUUID().toString(), "Child Care", "Child care is expensive and difficult to juggle with college."));
+		
+		return challengeTOs;
+	}
+	
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+	public @ResponseBody ChallengeTO getAll(@PathVariable String id) throws Exception {
+		return new ChallengeTO(UUID.randomUUID().toString(), "Child Care", "Child care is expensive and difficult to juggle with college.");
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public @ResponseBody ChallengeTO save(@RequestParam("name") String name, @RequestParam("description") String description) throws Exception {
+		return new ChallengeTO(UUID.randomUUID().toString(), "Child Care", "Child care is expensive and difficult to juggle with college.");
+	}
+	
+}
