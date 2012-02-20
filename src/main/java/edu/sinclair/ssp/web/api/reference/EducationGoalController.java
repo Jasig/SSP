9b2a6aff33b3d1232c07deaa14c3003f59d888f9
Daniel Bower <daniel.d.bower@gmail.com>
2012-02-20 22:34:35 +0000

    add educationGoal reference data
    git-svn-id: https://studentzen.svn.cvsdude.com/sspopensource/SSP/trunk@32 14c17786-f9a5-455e-acda-2e4726e2ef49

---
+++ src/main/java/edu/sinclair/ssp/web/api/reference/EducationGoalController.java
@@ -0,0 +1,54 @@
+package edu.sinclair.ssp.web.api.reference;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.sinclair.ssp.model.reference.EducationGoal;
+import edu.sinclair.ssp.service.reference.EducationGoalService;
+
+public class EducationGoalController implements
+		ReferenceController<EducationGoal> {
+
+	private static final Logger logger = LoggerFactory.getLogger(EducationGoalController.class);
+
+	@Autowired
+	private EducationGoalService service;
+	
+	@Override
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public List<EducationGoal> getAll() throws Exception {
+		return service.getAll();
+	}
+	
+	@Override
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+	public EducationGoal get(UUID id) throws Exception {
+		return service.get(id);
+	}
+	
+	@Override
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public EducationGoal save(EducationGoal obj, BindingResult result)
+			throws Exception {
+		if(result.hasErrors()){
+			logger.debug("There were " + result.getErrorCount() + " errors.");
+			return null;
+		}
+		
+		return service.save(obj);
+	}
+
+	@Override
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+	public void delete(UUID id) throws Exception {
+		service.delete(id);
+	}
+
+}
