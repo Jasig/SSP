644e0f4ffe9126cad03a8f0b9cf81e3886a25f10
Shawn Gormley <shawn.gormly@sinclair.edu>
2012-03-01 21:34:56 +0000

    git-svn-id: https://studentzen.svn.cvsdude.com/sspopensource/SSP/trunk@56 14c17786-f9a5-455e-acda-2e4726e2ef49

--- src/main/webapp/app/store/tool/studentintake/StudentIntakeForm.js
+++
@@ -1,7 +0,0 @@
-Ext.define('Ssp.store.tool.studentintake.StudentIntakeForm', {
-    extend: 'Ext.data.Store',
-    model: 'Ssp.model.tool.StudentIntakeFormTO',
-    storeId: 'studentIntakeFormStore',
-	autoLoad: false
-	
-});
