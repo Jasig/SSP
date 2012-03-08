128169e7bd739d14b7785f15475cfb57787f5e08
Daniel Bower <daniel.d.bower@gmail.com>
2012-03-08 21:29:32 +0000

    First pass at session controller
    git-svn-id: https://studentzen.svn.cvsdude.com/sspopensource/SSP/trunk@98 14c17786-f9a5-455e-acda-2e4726e2ef49

---
+++ src/main/java/edu/sinclair/ssp/web/api/SessionController.java
@@ -0,0 +1,36 @@
+package edu.sinclair.ssp.web.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.sinclair.ssp.security.SspUser;
+import edu.sinclair.ssp.service.SecurityService;
+import edu.sinclair.ssp.transferobject.ServiceResponse;
+
+
+@Controller
+@RequestMapping("/session")
+public class SessionController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SessionController.class);
+
+	@Autowired
+	private SecurityService service;
+	
+	@RequestMapping(value="getAuthenticatedPerson", method = RequestMethod.GET)
+	public @ResponseBody SspUser getAuthenticatedPerson() throws Exception {
+		return service.currentlyLoggedInSspUser();
+	}
+
+	@ExceptionHandler(Exception.class)
+	public @ResponseBody ServiceResponse handle(Exception e){
+		logger.error("Error: ", e);
+		return new ServiceResponse(false, e.getMessage());
+	}
+}
