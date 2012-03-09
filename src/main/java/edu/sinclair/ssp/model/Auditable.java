1bd25491d85d24abc4df7754a7447d28cb2a9782
Daniel Bower <daniel.d.bower@gmail.com>
2012-03-09 15:30:44 +0000

    Extract Auditable Properties into a separate abstract class
    git-svn-id: https://studentzen.svn.cvsdude.com/sspopensource/SSP/trunk@103 14c17786-f9a5-455e-acda-2e4726e2ef49

---
+++ src/main/java/edu/sinclair/ssp/model/Auditable.java
@@ -0,0 +1,117 @@
+package edu.sinclair.ssp.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+@MappedSuperclass
+public abstract class Auditable {
+	// jpa
+	@Id
+	@Type(type="pg-uuid")
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private UUID id;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_date", nullable = false, updatable = false)
+	private Date createdDate;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "created_by", nullable = false, updatable = false)
+	private Person createdBy;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "modified_date", nullable = false)
+	private Date modifiedDate;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "modified_by", nullable = false)
+	private Person modifiedBy;
+	
+	@Enumerated
+	@Column(name = "object_status", nullable = false)
+	private ObjectStatus objectStatus;
+
+	/**
+	@Version
+	private int optlock;*/
+
+
+	public Auditable(){}
+
+	public Auditable(UUID id){
+		this.id = id;
+	}
+
+
+	public UUID getId() {
+		return id;
+	}
+	
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Person getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Person createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public Person getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(Person modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public ObjectStatus getObjectStatus() {
+		return objectStatus;
+	}
+
+	public void setObjectStatus(ObjectStatus objectStatus) {
+		this.objectStatus = objectStatus;
+	}
+
+	public String toString(){
+		if(null!=id){
+			return id.toString();
+		}else{
+			return super.toString();
+		}
+	}
+}
--- src/main/java/edu/sinclair/ssp/model/reference/AbstractReference.java
+++ src/main/java/edu/sinclair/ssp/model/reference/AbstractReference.java
@@ -1,37 +1,18 @@
 package edu.sinclair.ssp.model.reference;
 
-import java.util.Date;
 import java.util.UUID;
 
 import javax.persistence.Column;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.NotEmpty;
 
-import edu.sinclair.ssp.model.ObjectStatus;
-import edu.sinclair.ssp.model.Person;
+import edu.sinclair.ssp.model.Auditable;
 
 @MappedSuperclass
-public abstract class AbstractReference {
+public abstract class AbstractReference extends Auditable {
 
-	// jpa
-	@Id
-	@Type(type="pg-uuid")
-	@GeneratedValue(generator = "uuid")
-	@GenericGenerator(name = "uuid", strategy = "uuid2")
-	private UUID id;
-	
 	//jpa
 	@Column(name = "name", nullable = false, length=100)
 	//validator
@@ -41,54 +22,23 @@ public abstract class AbstractReference {
 	
 	private String description;
 	
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "created_date")
-	private Date createdDate;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "created_by", nullable = false, updatable = false)
-	private Person createdBy;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "modified_date")
-	private Date modifiedDate;
-	
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "modified_by", nullable = false, updatable = false)
-	private Person modifiedBy;
-	
-	@Enumerated
-	@Column(name = "object_status")
-	private ObjectStatus objectStatus;
-
-	/**
-	@Version
-	private int optlock;*/
-	
-	public AbstractReference() {}
+	public AbstractReference() {
+		super();
+	}
 	
 	public AbstractReference(UUID id){
-		this.id = id;
+		super(id);
 	}
 	
 	public AbstractReference(UUID id, String name) {
-		this.id = id;
+		super(id);
 		this.name = name;
 	}
 	
 	public AbstractReference(UUID id, String name, String description) {
-		this.id = id;
+		super(id);
 		this.name = name;
 		this.description = description;
-	}
-	
-	public UUID getId() {
-		return id;
-	}
-	
-	public void setId(UUID id) {
-		this.id = id;
 	}
 	
 	public String getName() {
@@ -107,52 +57,4 @@ public abstract class AbstractReference {
 		this.description = description;
 	}
 
-	public Date getCreatedDate() {
-		return createdDate;
-	}
-
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
-
-	public Person getCreatedBy() {
-		return createdBy;
-	}
-
-	public void setCreatedBy(Person createdBy) {
-		this.createdBy = createdBy;
-	}
-
-	public Date getModifiedDate() {
-		return modifiedDate;
-	}
-
-	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
-
-	public Person getModifiedBy() {
-		return modifiedBy;
-	}
-
-	public void setModifiedBy(Person modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-
-	public ObjectStatus getObjectStatus() {
-		return objectStatus;
-	}
-
-	public void setObjectStatus(ObjectStatus objectStatus) {
-		this.objectStatus = objectStatus;
-	}
-
-	public String toString(){
-		if(null!=id){
-			return id.toString();
-		}else{
-			return super.toString();
-		}
-	}
-	
 }
