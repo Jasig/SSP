045b318fa48cafd5bc1d708489244f66318a4e87
Daniel Bower <daniel.d.bower@gmail.com>
2012-03-14 20:26:53 +0000

    Added environment-specific path support for LogBackConfigLoaderTest.
    git-svn-id: https://studentzen.svn.cvsdude.com/sspopensource/SSP/trunk@115 14c17786-f9a5-455e-acda-2e4726e2ef49

--- src/test/java/edu/sinclair/ssp/config/logging/LogBackConfigLoaderTest.java
+++ src/test/java/edu/sinclair/ssp/config/logging/LogBackConfigLoaderTest.java
@@ -1,27 +1,44 @@
 package edu.sinclair.ssp.config.logging;
+
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import edu.sinclair.ssp.config.logging.LogBackConfigLoader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 public class LogBackConfigLoaderTest {
 
-	private Logger logger = LoggerFactory.getLogger(LogBackConfigLoaderTest.class);
-	
+	private Logger logger = LoggerFactory
+			.getLogger(LogBackConfigLoaderTest.class);
+
+	private Properties config;
+
+	@Before
+	public void before() throws IOException {
+		config = new Properties();
+
+		Resource file = new ClassPathResource("testConfig.properties");
+		config.load(file.getInputStream());
+	}
+
 	@Test
 	public void test() {
-		try{
-			assertNotNull(new LogBackConfigLoader("/usr/local/etc/ssp/logback.xml"));
-		}catch(Exception e){
+		try {
+			assertNotNull(new LogBackConfigLoader(
+					config.getProperty("testConfigDirectory") + "logback.xml"));
+		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
-		
+
 		logger.info("Testing logger location");
 	}
-
 }
