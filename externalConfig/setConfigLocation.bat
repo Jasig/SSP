bcaf4b21d2d842f7ede4e520b6dc2d5e0cfc0ce4
Daniel Bower <daniel.d.bower@gmail.com>
2012-03-15 15:32:22 +0000

    support scripts for setting environment variables
    git-svn-id: https://studentzen.svn.cvsdude.com/sspopensource/SSP/trunk@118 14c17786-f9a5-455e-acda-2e4726e2ef49

---
+++ externalConfig/setConfigLocation.bat
@@ -0,0 +1,6 @@
+set SSP_TESTCONFIGDIR=C:\\sspConf\\
+set SSP_CONFIGDIR=C:\\sspConf\\
+
+echo The test configuration file for SSP should be in:
+echo %SSP_TESTCONFIGDIR%
+echo Please verify the file exists and is populated with the correct values.
---
+++ externalConfig/setConfigLocation.sh
@@ -0,0 +1,6 @@
+export SSP_TESTCONFIGDIR=/usr/local/etc/ssp/
+export SSP_CONFIGDIR=/usr/local/etc/ssp/
+
+echo The configuration file for SSP should be in:
+echo $SSP_TESTCONFIGDIR
+echo Please verify the file exists and is populated with the correct values.
