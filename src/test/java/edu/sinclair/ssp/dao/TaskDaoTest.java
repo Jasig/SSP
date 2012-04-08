e179e0b9d093e2d8cbe3586ecbab1e66632798df
Daniel Bower <daniel.d.bower@gmail.com>
2012-04-07 22:40:07 -0400

    stub in some tests

---
+++ src/test/java/edu/sinclair/ssp/dao/TaskDaoTest.java
@@ -0,0 +1,21 @@
+package edu.sinclair.ssp.dao;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TaskDaoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Ignore
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
