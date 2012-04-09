b01e88112d970bb7867ca08cee1a084720e0c5bf
Daniel Bower <daniel.d.bower@gmail.com>
2012-04-08 22:34:24 -0400

    remove some methods that are no longer used in MyGpsStringUtils in preparation for porting class

--- src/main/java/edu/sinclair/mygps/util/MyGpsStringUtils.java
+++ src/main/java/edu/sinclair/mygps/util/MyGpsStringUtils.java
@@ -4,41 +4,32 @@ import java.util.UUID;
 
 public class MyGpsStringUtils {
 
-	public static String stringFromYear(int year){
-		if(0==year){
+	public static String stringFromYear(int year) {
+		if (0 == year) {
 			return "";
-		}else{
+		} else {
 			return String.valueOf(year);
 		}
 	}
-	
-	public static String stringFromBoolean(boolean val){
-		if(val){
+
+	public static String stringFromBoolean(boolean val) {
+		if (val) {
 			return "true";
-		}else{
+		} else {
 			return "";
 		}
 	}
-	
-	public static boolean booleanFromString(String val){
-		if(val.equalsIgnoreCase("true")){
+
+	public static boolean booleanFromString(String val) {
+		if (val.equalsIgnoreCase("true")) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
-	
-	public static UUID uuidFromColdfusionString(String uuid){
-		String fixDashes = uuid.substring(0,23) + "-" + uuid.substring(23,35);
-		return UUID.fromString(fixDashes);
-	}
 
-	public static String coldfusionStringFromUUID(UUID uuid){
+	public static String coldfusionStringFromUUID(UUID uuid) {
 		String uuidString = uuid.toString().toUpperCase();
-		return uuidString.substring(0,23) + uuidString.substring(24,36);
-	}
-	
-	public static int compareStandardUuidAndColdfusionString(UUID a, String b){
-		return a.compareTo(uuidFromColdfusionString(b));
+		return uuidString.substring(0, 23) + uuidString.substring(24, 36);
 	}
 }
