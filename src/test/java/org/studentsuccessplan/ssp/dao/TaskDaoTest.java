b82f31f4d46ca5ed79c3d9216c836de7404d7957
Daniel Bower <daniel.d.bower@gmail.com>
2012-04-11 10:17:24 -0400

    test for custom task dao

--- src/test/java/org/studentsuccessplan/ssp/dao/TaskDaoTest.java
+++ src/test/java/org/studentsuccessplan/ssp/dao/TaskDaoTest.java
@@ -1,18 +1,12 @@
 package org.studentsuccessplan.ssp.dao;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import java.util.Calendar;
-import java.util.List;
 import java.util.UUID;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -20,28 +14,26 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.studentsuccessplan.ssp.dao.reference.ChallengeDao;
 import org.studentsuccessplan.ssp.dao.reference.ChallengeReferralDao;
-import org.studentsuccessplan.ssp.model.Person;
 import org.studentsuccessplan.ssp.model.Task;
 import org.studentsuccessplan.ssp.model.reference.Challenge;
 import org.studentsuccessplan.ssp.model.reference.ChallengeReferral;
-import org.studentsuccessplan.ssp.service.ObjectNotFoundException;
-import org.studentsuccessplan.ssp.service.PersonService;
-import org.studentsuccessplan.ssp.service.impl.SecurityServiceInTestEnvironment;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("reference/dao-testConfig.xml")
 @TransactionConfiguration(defaultRollback = false)
 @Transactional
-public class TaskDaoTest {
+public class TaskDaoTest extends AbstractTaskDaoTest<Task> {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(TaskDaoTest.class);
+	// private static final Logger LOGGER = LoggerFactory
+	// .getLogger(TaskDaoTest.class);
 
 	@Autowired
 	private TaskDao dao;
 
-	@Autowired
-	private PersonService personService;
+	@Override
+	protected TaskDao getDao() {
+		return dao;
+	}
 
 	@Autowired
 	private ChallengeDao challengeDao;
@@ -49,22 +41,14 @@ public class TaskDaoTest {
 	@Autowired
 	private ChallengeReferralDao challengeReferralDao;
 
-	private Person ken;
 	private Challenge testChallenge;
 	private ChallengeReferral testChallengeReferral;
 	private Task testTask;
 
-	@Autowired
-	private SecurityServiceInTestEnvironment securityService;
-
+	@Override
 	@Before
 	public void setUp() {
-		try {
-			ken = personService.personFromUsername("ken");
-		} catch (ObjectNotFoundException e) {
-			LOGGER.error("can't find one of either sysadmin or ken");
-		}
-		securityService.setCurrent(ken);
+		super.setUp();
 
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.DAY_OF_YEAR, 7);
@@ -81,41 +65,6 @@ public class TaskDaoTest {
 	@After
 	public void destroy() {
 		dao.delete(testTask);
-	}
-
-	@Test
-	public void getAllForPersonId() {
-		assertList(dao.getAllForPersonId(ken.getId()));
-	}
-
-	@Test
-	public void getAllForPersonId_complete() {
-		assertList(dao.getAllForPersonId(ken.getId(), true));
-	}
-
-	@Test
-	public void getAllForPersonId_incomplete() {
-		assertList(dao.getAllForPersonId(ken.getId(), false));
-	}
-
-	@Test
-	public void getAllForSessionId() {
-		assertList(dao.getAllForSessionId("test session id"));
-	}
-
-	@Test
-	public void getAllForSessionId_complete() {
-		assertList(dao.getAllForSessionId("test session id", true));
-	}
-
-	@Test
-	public void getAllForSessionId_incomplete() {
-		assertList(dao.getAllForSessionId("test session id", false));
-	}
-
-	@Test
-	public void getAllWhichNeedRemindersSent() {
-		assertList(dao.getAllWhichNeedRemindersSent());
 	}
 
 	/*
@@ -137,10 +86,4 @@ public class TaskDaoTest {
 				"test sessionId", true, testChallengeReferral.getId()));
 	}
 
-	private void assertList(List<Task> objects) {
-		for (Task object : objects) {
-			assertNotNull(object.getId());
-		}
-		assertTrue(true);
-	}
 }
