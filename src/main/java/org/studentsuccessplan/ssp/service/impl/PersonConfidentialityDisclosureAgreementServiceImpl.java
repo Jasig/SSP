331464e9f594a30654f1657c729d7f1a976e52f1
Jon Adams <jadams@firetiger.net>
2012-04-17 22:35:45 -0400

    MyGPS: Some StudentIntake changes to fit the new SSP model

--- src/main/java/org/studentsuccessplan/ssp/service/impl/PersonConfidentialityDisclosureAgreementServiceImpl.java
+++ src/main/java/org/studentsuccessplan/ssp/service/impl/PersonConfidentialityDisclosureAgreementServiceImpl.java
@@ -1,22 +1,26 @@
 package org.studentsuccessplan.ssp.service.impl;
 
+import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import org.studentsuccessplan.ssp.dao.PersonConfidentialityDisclosureAgreementDao;
 import org.studentsuccessplan.ssp.dao.reference.ConfidentialityDisclosureAgreementDao;
 import org.studentsuccessplan.ssp.model.ObjectStatus;
 import org.studentsuccessplan.ssp.model.Person;
 import org.studentsuccessplan.ssp.model.PersonConfidentialityDisclosureAgreement;
 import org.studentsuccessplan.ssp.model.reference.ConfidentialityDisclosureAgreement;
+import org.studentsuccessplan.ssp.service.ObjectNotFoundException;
 import org.studentsuccessplan.ssp.service.PersonConfidentialityDisclosureAgreementService;
+
+import com.google.common.collect.Sets;
 
 @Service
 @Transactional(readOnly = true)
-public class PersonConfidentialityDisclosureAgreementServiceImpl implements PersonConfidentialityDisclosureAgreementService {
+public class PersonConfidentialityDisclosureAgreementServiceImpl implements
+		PersonConfidentialityDisclosureAgreementService {
 
 	@Autowired
 	private PersonConfidentialityDisclosureAgreementDao dao;
@@ -24,60 +28,46 @@ public class PersonConfidentialityDisclosureAgreementServiceImpl implements Pers
 	@Autowired
 	private ConfidentialityDisclosureAgreementDao cdaDao;
 
-	/**
-	 * Has the student agreed to the most recent
-	 * ConfidentialityDisclosureAgreement
-	 */
 	@Override
 	public PersonConfidentialityDisclosureAgreement hasStudentAgreedToLatest(
 			Person student) {
 		throw new RuntimeException("not implemented");
 	}
 
-	/**
-	 * Has the student agreed to at least one
-	 * ConfidentialityDisclosureAgreement
-	 */
 	@Override
 	public PersonConfidentialityDisclosureAgreement hasStudentAgreedToOne(
 			Person student) {
 		List<PersonConfidentialityDisclosureAgreement> agreements = dao
 				.forStudent(student);
-		if ((agreements != null) && (agreements.size() > 0)) {
+
+		if ((agreements != null) && !agreements.isEmpty()) {
 			return agreements.get(0);
-		} else {
-			return null;
 		}
+
+		return null;
 	}
 
-	/**
-	 * Has the student agreed to a specific agreement?
-	 */
 	@Override
 	public PersonConfidentialityDisclosureAgreement hasStudentAgreedTo(
 			Person student, ConfidentialityDisclosureAgreement agreement) {
 		return dao.forStudentAndAgreement(student, agreement);
 	}
 
-	/**
-	 * Return the most recent agreement
-	 */
 	@Override
-	public ConfidentialityDisclosureAgreement latestAgreement() {
-		List<ConfidentialityDisclosureAgreement> agreements =
-				cdaDao.getAll(ObjectStatus.ACTIVE);
-		if ((agreements != null) && (agreements.size() > 0)) {
+	public ConfidentialityDisclosureAgreement latestAgreement()
+			throws ObjectNotFoundException {
+		List<ConfidentialityDisclosureAgreement> agreements = cdaDao.getAll(
+				ObjectStatus.ACTIVE, 0, 1, "modifiedDate", "DESC");
+		if ((agreements != null) && (!agreements.isEmpty())) {
 			return agreements.get(0);
 		} else {
-			return null;
+			throw new ObjectNotFoundException(
+					"Latest Confidentiality Agreement could not be found.");
 		}
 		// :TODO should we create an active flag on
 		// ConfidentialityDisclosureAgreement?
 	}
 
-	/**
-	 * Mark the given student as agreeing to the agreement
-	 */
 	@Override
 	public void studentAgreedTo(Person student,
 			ConfidentialityDisclosureAgreement agreement) {
@@ -86,15 +76,20 @@ public class PersonConfidentialityDisclosureAgreementServiceImpl implements Pers
 			PersonConfidentialityDisclosureAgreement pAgreement = new PersonConfidentialityDisclosureAgreement();
 			pAgreement.setConfidentialityDisclosureAgreement(agreement);
 			pAgreement.setPerson(student);
-			dao.save(pAgreement);
+			pAgreement = dao.save(pAgreement);
+
+			if (student.getConfidentialityDisclosureAgreements() == null) {
+				HashSet<PersonConfidentialityDisclosureAgreement> confidentialityDisclosureAgreementsSet = Sets
+						.newHashSet();
+				student.setConfidentialityDisclosureAgreements(confidentialityDisclosureAgreementsSet);
+			}
+
+			student.getConfidentialityDisclosureAgreements().add(pAgreement);
 		}// otherwise ignore
 	}
 
-	/**
-	 * Mark the given student as agreeing to the most recent agreement
-	 */
 	@Override
-	public void studentAgreed(Person student) {
+	public void studentAgreed(Person student) throws ObjectNotFoundException {
 		studentAgreedTo(student, latestAgreement());
 	}
 }
