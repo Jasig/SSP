d1ec3575b7caf0ff781a45c2f822c0f4b5e6bbab
Daniel Bower <daniel.d.bower@gmail.com>
2012-04-28 23:56:23 -0400

    soft delete a set

--- src/main/java/org/studentsuccessplan/ssp/util/SetOps.java
+++ src/main/java/org/studentsuccessplan/ssp/util/SetOps.java
@@ -2,11 +2,20 @@ package org.studentsuccessplan.ssp.util;
 
 import java.util.Set;
 
+import org.studentsuccessplan.ssp.model.Auditable;
+import org.studentsuccessplan.ssp.model.ObjectStatus;
+
 public class SetOps {
 
 	public static <T> Set<T> updateSet(Set<T> existing, Set<T> replacingWith) {
 		existing.clear();
 		existing.addAll(replacingWith);
 		return existing;
+	}
+
+	public static <T extends Auditable> void softDeleteSetItems(Set<T> items) {
+		for (T item : items) {
+			item.setObjectStatus(ObjectStatus.DELETED);
+		}
 	}
 }
