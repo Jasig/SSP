ebc1b44e2ff08112442559a0495aa46d89b68b10
Shawn Gormley <shawn.gormley@sinclair.edu>
2012-05-03 09:19:29 -0400

    Adjust package names to org.jasig.

--- src/main/java/org/jasig/ssp/portlet/AbstractSSPController.java
+++ src/main/java/org/jasig/ssp/portlet/AbstractSSPController.java
@@ -1,4 +1,4 @@
-package org.studentsuccessplan.ssp.portlet;
+package org.jasig.ssp.portlet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/org/jasig/ssp/portlet/SSPIndexPortletController.java
+++ src/main/java/org/jasig/ssp/portlet/SSPIndexPortletController.java
@@ -1,4 +1,4 @@
-package org.studentsuccessplan.ssp.portlet;
+package org.jasig.ssp.portlet;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
