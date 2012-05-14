b5f9c51329f3dbe375009763177e4ef2f99de4e5
Daniel Bower <daniel.d.bower@gmail.com>
2012-05-14 16:09:41 -0400

    add some docs on local configs

--- src/main/config/devTools/setConfigLocation.bat
+++ src/main/config/devTools/setConfigLocation.bat
@@ -1,5 +1,6 @@
 # This script is only needed in a development environment for setting the environment
-# variable that has the location of the test configuration directory.
+# variable that has the location of the test configuration directory for your console.
+# Usage scenarios:  before running mvn, or in tomcat startup script
 #
 # This is the windows version of the script.
 #
@@ -8,6 +9,9 @@
 
 set SSP_CONFIGDIR=C:\\sspConf\\
 set SSP_TESTCONFIGDIR=C:\\sspConf\\
+
+# Uncomment the following to allow SSP to run in absence of uportal in the dev environment
+#set spring.profiles.active=dev-standalone
 
 echo The test configuration file for SSP should be in:
 echo %SSP_TESTCONFIGDIR% for the TEST environment, and %SSP_CONFIGDIR% for the DEPLOY environment
--- src/main/config/devTools/setConfigLocation.sh
+++ src/main/config/devTools/setConfigLocation.sh
@@ -1,5 +1,6 @@
 # This script is only needed in a development environment for setting the environment
-# variable that has the location of the test configuration directory.
+# variable that has the location of the test configuration directory for your console.
+# Usage scenarios:  before running mvn, or in tomcat startup script
 #
 # This is the unix version of the script.  It has been tested with bash.
 #
@@ -8,6 +9,9 @@
 
 export SSP_CONFIGDIR=/usr/local/etc/ssp/
 export SSP_TESTCONFIGDIR=/usr/local/etc/ssp/
+
+# Uncomment the following to allow SSP to run in absence of uportal in the dev environment
+#export spring.profiles.active=dev-standalone
 
 echo The configuration file for SSP should be in:
 echo $SSP_TESTCONFIGDIR for the TEST environment, and $SSP_CONFIGDIR for the DEPLOY environment
