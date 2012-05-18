9baad9ba3684dd8d972afc1d211f319198b56b37
Daniel Bower <daniel.d.bower@gmail.com>
2012-05-18 15:57:58 -0400

    remove unused field

--- src/main/java/org/jasig/ssp/service/reference/impl/ChallengeServiceImpl.java
+++ src/main/java/org/jasig/ssp/service/reference/impl/ChallengeServiceImpl.java
@@ -2,7 +2,6 @@ package org.jasig.ssp.service.reference.impl;
 
 import java.util.List;
 
-import org.jasig.ssp.dao.reference.ChallengeCategoryDao;
 import org.jasig.ssp.dao.reference.ChallengeDao;
 import org.jasig.ssp.model.reference.Category;
 import org.jasig.ssp.model.reference.Challenge;
@@ -27,9 +26,6 @@ public class ChallengeServiceImpl extends AbstractReferenceService<Challenge>
 
 	@Autowired
 	private transient ChallengeReferralService challengeReferralService;
-
-	@Autowired
-	private transient ChallengeCategoryDao challengeCategoryDao;
 
 	@Autowired
 	private transient SecurityService securityService;
