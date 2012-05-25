34037bf2e7e67921f23a1b0cff4acf76bdce18cb
Daniel Bower <daniel.d.bower@gmail.com>
2012-05-25 10:22:09 -0400

    fix some pmd errors and create a new Exception for security - may need some tweaking

--- src/main/java/org/jasig/ssp/service/impl/UPortalPersonAttributesService.java
+++ src/main/java/org/jasig/ssp/service/impl/UPortalPersonAttributesService.java
@@ -11,6 +11,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.jasig.portlet.utils.rest.CrossContextRestApiInvoker;
 import org.jasig.portlet.utils.rest.RestResponse;
 import org.jasig.portlet.utils.rest.SimpleCrossContextRestApiInvoker;
+import org.jasig.ssp.security.exception.UPortalSecurityException;
 import org.jasig.ssp.service.ObjectNotFoundException;
 import org.jasig.ssp.service.PersonAttributesService;
 import org.slf4j.Logger;
@@ -21,51 +22,58 @@ import org.springframework.stereotype.Service;
 public class UPortalPersonAttributesService implements PersonAttributesService {
 
 	private static final String PARAM_USERNAME = "username";
-	private static final String REST_URI = "/ssp-platform/api/people/{" 
-										+ PARAM_USERNAME + "}.json";
+	private static final String REST_URI = "/ssp-platform/api/people/{"
+			+ PARAM_USERNAME + "}.json";
 	private static final String PERSON_KEY = "person";
 	private static final String ATTRIBUTES_KEY = "attributes";
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(UPortalPersonAttributesService.class);
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public Map<String,List<String>> getAttributes(HttpServletRequest req, HttpServletResponse res, String username) throws ObjectNotFoundException {
-		
-		log.debug("Fetching attributes for user '{}'", username);
+	public Map<String, List<String>> getAttributes(
+			final HttpServletRequest req,
+			final HttpServletResponse res, final String username)
+			throws ObjectNotFoundException {
+
+		LOGGER.debug("Fetching attributes for user '{}'", username);
 
 		final Map<String, String[]> params = new HashMap<String, String[]>();
-		params.put(PARAM_USERNAME, new String[] {username});
+		params.put(PARAM_USERNAME, new String[] { username });
 
-		CrossContextRestApiInvoker rest = new SimpleCrossContextRestApiInvoker();
-		RestResponse rr = rest.invoke(req, res, REST_URI, params);
-		
-		ObjectMapper mapper = new ObjectMapper();
-		Map<String,Map<String,Map<String,List<String>>>> value = null;
+		final CrossContextRestApiInvoker rest = new SimpleCrossContextRestApiInvoker();
+		final RestResponse rr = rest.invoke(req, res, REST_URI, params);
+
+		final ObjectMapper mapper = new ObjectMapper();
+		Map<String, Map<String, Map<String, List<String>>>> value = null;
 		try {
 			value = mapper.readValue(rr.getWriterOutput(), Map.class);
 		} catch (Exception e) {
-			String msg = "Failed to access attributes for the specified person:  " 
-																+ username;
-			throw new RuntimeException(msg, e);
+			final String msg = "Failed to access attributes for the specified person:  "
+					+ username;
+			throw new UPortalSecurityException(msg, e);
 		}
 
-		Map<String,Map<String,List<String>>> person = value.get(PERSON_KEY);
+		final Map<String, Map<String, List<String>>> person = value
+				.get(PERSON_KEY);
 		if (person == null) {
 			// No match...
-			String msg = "The specified person is unrecognized";
-			throw new ObjectNotFoundException(msg, username);
+			throw new ObjectNotFoundException(
+					"The specified person is unrecognized", username);
 		}
-		Map<String,List<String>> rslt = person.get(ATTRIBUTES_KEY);
+		final Map<String, List<String>> rslt = person.get(ATTRIBUTES_KEY);
 		if (rslt == null) {
-			String msg = "No attributes are available for the specified person";
-			throw new ObjectNotFoundException(msg, username);
+			throw new ObjectNotFoundException(
+					"No attributes are available for the specified person",
+					username);
 		}
-		
-		log.debug("Retrieved the following attributes for user {}:  {}", username, rslt.toString());
+
+		LOGGER.debug("Retrieved the following attributes for user {}:  {}",
+				username, rslt.toString());
 
 		return rslt;
-		
+
 	}
 
 }
