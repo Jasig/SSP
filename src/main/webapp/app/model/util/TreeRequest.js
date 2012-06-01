2e615cdefedefba00e3d540988615fd49d57a42b
Shawn Gormley <shawn.gormley@sinclair.edu>
2012-06-01 15:05:21 -0400

    Move viewHistory method to ToolViewController for access from multiple tools.  Add functionality for Journal Entries to be Added, Edited and Deleted.  Add Tree view for Journal Detail selection.  Move docked viewHistory buttons to top of panels.

--- src/main/webapp/app/model/util/TreeRequest.js
+++ src/main/webapp/app/model/util/TreeRequest.js
@@ -5,5 +5,9 @@ Ext.define('Ssp.model.util.TreeRequest', {
              {name: 'isLeaf', type: 'boolean'},
              {name: 'nodeToAppendTo', defaultValue: null},
              {name: 'destroyBeforeAppend', type: 'boolean', defaultValue: false},
-             {name: 'enableCheckedItems', type: 'boolean', defaultValue: true}]
+             {name: 'enableCheckedItems', type: 'boolean', defaultValue: true},
+             {name: 'expanded', type:'boolean',defaultValue: false},
+             {name: 'expandable', type:'boolean', deafultValue: true},
+             {name: 'callbackFunc',type:'auto'},
+             {name: 'callbackScope', type: 'auto'}]
 });
