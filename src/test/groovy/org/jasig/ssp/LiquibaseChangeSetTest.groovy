2214f3de7115d23f99d2e0accc1be2951519093b
Daniel Bower <daniel.d.bower@gmail.com>
2012-06-04 10:40:02 -0400

    stub in LiquibaseChangeSetTest

---
+++ src/test/groovy/org/jasig/ssp/LiquibaseChangeSetTest.groovy
@@ -0,0 +1,28 @@
+package org.jasig.ssp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test
+
+
+class LiquibaseChangeSetTest {
+
+	@Test
+	void main(){
+		//read config values from file
+		//create connection to test database
+		//count number of changeSets Applied
+		//roll all changeSets back
+
+		//apply production changeSets
+
+		//if made it here, production changeSets succeeded
+
+		//if not, roll them all back and fail
+
+		//apply test changeSets
+		//if made it here, test changeSets succeeded
+		//if not, roll them all back and fail
+
+	}
+}
