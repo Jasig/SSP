4ee8d1f37f1fe97f30df9f4577aa6e9c62dbe80c
Daniel Bower <daniel.d.bower@gmail.com>
2012-06-19 14:59:53 -0400

    Add authorities processing to Uportal PreAuth Processing Filter

--- src/main/java/org/jasig/ssp/security/uportal/UPortalPreAuthenticatedProcessingFilter.java
+++ src/main/java/org/jasig/ssp/security/uportal/UPortalPreAuthenticatedProcessingFilter.java
@@ -1,12 +1,14 @@
 package org.jasig.ssp.security.uportal;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedCredentialsNotFoundException;
 
 public class UPortalPreAuthenticatedProcessingFilter extends
@@ -50,6 +52,26 @@ public class UPortalPreAuthenticatedProcessingFilter extends
 			final HttpServletRequest request) {
 		// credentials aren't passed from uportal, just the principal
 		return "N/A";
+	}
+
+	@Override
+	protected void successfulAuthentication(final HttpServletRequest request,
+			final HttpServletResponse response, final Authentication authResult) {
+
+		// pull the Authentication token from the portlet session
+		final HttpSession session = request.getSession();
+		final Authentication sessionToken = (Authentication) session
+				.getAttribute(UPortalSecurityFilter.AUTHENTICATION_TOKEN_KEY);
+
+		// Recreate the PreAuthenticatedAuthenticationToken and set authorities
+		final PreAuthenticatedAuthenticationToken token =
+				new PreAuthenticatedAuthenticationToken(
+						authResult.getPrincipal(), authResult.getCredentials(),
+						sessionToken.getAuthorities());
+		token.setAuthenticated(authResult.isAuthenticated());
+		token.setDetails(authResult.getDetails());
+
+		super.successfulAuthentication(request, response, token);
 	}
 
 }
