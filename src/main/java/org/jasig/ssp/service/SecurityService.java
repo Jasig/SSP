0ab03e383051b444d6f68d0db23634f49de41a76
Jon Adams <jadams@firetiger.net>
2012-07-20 15:50:04 -0400

    Fix missing permission issue with Instructions API.

--- src/main/java/org/jasig/ssp/service/SecurityService.java
+++ src/main/java/org/jasig/ssp/service/SecurityService.java
@@ -66,7 +66,7 @@ public interface SecurityService {
 	/**
 	 * Return the current user, or if null, the admin
 	 * 
-	 * @return
+	 * @return the current user, or if null, the admin
 	 */
 	SspUser currentFallingBackToAdmin();
 }
