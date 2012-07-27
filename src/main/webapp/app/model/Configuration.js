b28ebd71248fad11c11b34a61e705ee33f9513b2
Shawn Gormley <shawn.gormley@sinclair.edu>
2012-07-27 09:59:37 -0400

    Implement a method to load a record from the external_data source by schoolId per issue SSP-326. Document configuration settings.

--- src/main/webapp/app/model/Configuration.js
+++ src/main/webapp/app/model/Configuration.js
@@ -1,61 +1,131 @@
 Ext.define('Ssp.model.Configuration', {
     extend: 'Ext.data.Model',
-    fields: [{name: 'syncStudentPersonalDataWithExternalData', 
+	fields: [
+	         /*
+		      * Set this option to true to lock editing on fields that relate to the external_data
+		      * source. This will restrict you from editing any data that relates to external after
+		      * you have first added a record to the system. All editing will be disabled for personal
+		      * data, including changing a studentId/schoolId after the record has been added to the
+		      * system.  
+		      */
+             {name: 'syncStudentPersonalDataWithExternalData', 
     	      type: 'boolean', 
     	      defaultValue: false
     	     },
+    	     /*
+    	      * Set this option to true to display the retrieveFromExternalDataButton on the Caseload
+    	      * Assignment Screen when adding a new record. This will allow you to populate a student's record
+    	      * from the external_data source while adding a new record to the system.
+    	      */
+    	     {
+    	    	name: 'allowExternalRetrievalOfStudentDataFromCaseloadAssignment',
+    	    	type: 'boolean',
+    	    	defaultValue: true
+    	     },
+    	     /*
+    	      * Set this option to true to lock editing of Coach Assignments for records in the system.
+    	      * When this option is set to true, all Coach Assignments will be populated from an external system
+    	      * through the external_data tables/views of this application.
+    	      */
     	     {name: 'coachSetFromExternalData', 
     	      type: 'boolean', 
     	      defaultValue: false
     	     },
+    	     /*
+    	      * Set this option to true to lock editing of Student Type Assignments for records in the system.
+    	      * When this option is set to true, all Student Type Assignments will be populated from an external system
+    	      * through the external_data tables/views of this application.
+    	      */
     	     {name: 'studentTypeSetFromExternalData', 
        	      type: 'boolean', 
        	      defaultValue: false
        	     },
+    	     /*
+    	      * Set this option to the label you would like to see for the studentId values in the system.
+    	      * For instance: Use this to label your studentId for your institution's naming convention.
+    	      */
              {
     	      name: 'studentIdAlias', 
     	      type: 'string', 
     	      defaultValue: 'Tartan ID'
     	     },
+    	     /*
+    	      * Minimum data length for a studentId/schoolId in the application.
+    	      */
     	     {name: 'studentIdMinValidationLength', 
     	      type: 'number', 
     	      defaultValue: 3
     	     },
+    	     /*
+    	      * Error message for a studentId/schoolId that exceeds the specified minimum validation length.
+    	      */
        	     {name: 'studentIdMinValidationErrorText', 
        	      type: 'string', 
        	      defaultValue: 'The entered value is not long enough.'
        	     },
+    	     /*
+    	      * Maximum data length for a studentId/schoolId in the application.
+    	      */
     	     {name: 'studentIdMaxValidationLength', 
        	      type: 'number', 
        	      defaultValue: 8
        	     },
+    	     /*
+    	      * Error message for a studentId/schoolId that exceeds the specified maximum validation length.
+    	      */
        	     {name: 'studentIdMaxValidationErrorText', 
       	      type: 'string', 
       	      defaultValue: 'The entered value is too long.'
       	     },
+    	     /*
+    	      * Values to validate for the allowable characters in a studentId/schoolId. This value will
+    	      * be converted and applied as a regular expression, so all regular expressions values should be
+    	      * available to this value.
+    	      * 
+    	      * For example: 
+    	      * 
+    	      * Only digits use "0-9".  
+    	      * Only alphabetical characters use: "a-zA-Z".
+    	      * Alphabetical characters and digits use: "a-zA-Z0-9"
+    	      */
     	     {name: 'studentIdAllowableCharacters', 
           	  type: 'string', 
           	  defaultValue: 'a-zA-Z0-9'
           	 },
+    	     /*
+    	      * Error message for a studentId/schoolId validation error.
+    	      */
     	     {name: 'studentIdValidationErrorText', 
              	  type: 'string', 
              	  defaultValue: 'Not a valid Student Id'
              },
+    	     /*
+    	      * Set to 1 to display the Employment Shift option on the Student Intake Tool.
+    	      */
     	     {
               name: 'displayStudentIntakeDemographicsEmploymentShift', 
               type: 'boolean', 
-              defaultValue: '1'
+              defaultValue: true
              },
+    	     /*
+    	      * Label to use for the Parent's Degree question on the Education Plan tab of the Student Intake Tool.
+    	      */
     	     {
               name: 'educationPlanParentsDegreeLabel', 
               type: 'string', 
               defaultValue: 'Have your parents obtained a college degree?'
              },
+    	     /*
+    	      * Label to use for the Special Needs question on the Education Plan tab of the Student Intake Tool.
+    	      */
     	     {
               name: 'educationPlanSpecialNeedsLabel', 
               type: 'string', 
               defaultValue: 'Special needs or require special accomodation?'
              },
+    	     /*
+    	      * Label to use for the Coach field displays across the application.
+    	      */
     	     {
               name: 'coachFieldLabel', 
               type: 'string', 
