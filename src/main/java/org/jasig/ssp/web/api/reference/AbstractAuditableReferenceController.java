bb552d9b2fe52100ba2f5e1c448b93602be82950
Daniel Bower <daniel.d.bower@gmail.com>
2012-08-08 14:50:23 -0400

    Resolve SSP-381 Things seem to be going awry on windows where a controller extends an abstract class with Request Mappings on it, removing the abstract class RestController because it wasn't really needed anyway.

--- src/main/java/org/jasig/ssp/web/api/reference/AbstractAuditableReferenceController.java
+++ src/main/java/org/jasig/ssp/web/api/reference/AbstractAuditableReferenceController.java
@@ -15,7 +15,7 @@ import org.jasig.ssp.transferobject.ServiceResponse;
 import org.jasig.ssp.transferobject.reference.AbstractReferenceTO;
 import org.jasig.ssp.util.sort.PagingWrapper;
 import org.jasig.ssp.util.sort.SortingAndPaging;
-import org.jasig.ssp.web.api.RestController;
+import org.jasig.ssp.web.api.AbstractBaseController;
 import org.jasig.ssp.web.api.validation.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,7 +40,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @PreAuthorize(Permission.SECURITY_REFERENCE_WRITE)
 public abstract class AbstractAuditableReferenceController<T extends AbstractReference, TO extends AbstractReferenceTO<T>>
-		extends RestController<TO, T> {
+		extends AbstractBaseController {
 
 	/**
 	 * Logger
@@ -84,7 +84,34 @@ public abstract class AbstractAuditableReferenceController<T extends AbstractRef
 		this.transferObjectClass = transferObjectClass;
 	}
 
-	@Override
+	/**
+	 * Retrieve every instance in the database filtered by the supplied status.
+	 * 
+	 * @param status
+	 *            Filter by this status.
+	 * @param start
+	 *            First result (0-based index) to return. Parameter must be a
+	 *            positive, non-zero integer. Often comes from client as a
+	 *            parameter labeled <code>start</code>. A null value indicates
+	 *            to return rows starting from index 0.
+	 * @param limit
+	 *            Maximum number of results to return. Parameter must be a
+	 *            positive, non-zero integer. Often comes from client as a
+	 *            parameter labeled <code>limit</code>. A null value indicates
+	 *            return all rows from the start parameter to the end of the
+	 *            data.
+	 * @param sort
+	 *            Property name. If null or empty string, the default sort will
+	 *            be used. If non-empty, must be a case-sensitive model property
+	 *            name. Often comes from client as a parameter labeled
+	 *            <code>sort</code>. Example sort expression:
+	 *            <code>propertyName</code>
+	 * @param sortDirection
+	 *            Ascending/descending keyword. If null or empty string, the
+	 *            default sort will be used. Must be <code>ASC</code> or
+	 *            <code>DESC</code>.
+	 * @return All entities in the database filtered by the supplied status.
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	@PreAuthorize(Permission.SECURITY_REFERENCE_READ)
 	public @ResponseBody
@@ -105,7 +132,17 @@ public abstract class AbstractAuditableReferenceController<T extends AbstractRef
 				.asTOList(data.getRows()));
 	}
 
-	@Override
+	/**
+	 * Retrieves the specified instance from persistent storage.
+	 * 
+	 * @param id
+	 *            The specific id to use to lookup the associated data.
+	 * @return The specified instance if found.
+	 * @throws ObjectNotFoundException
+	 *             If specified object could not be found.
+	 * @throws ValidationException
+	 *             If that specified data is not invalid.
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@PreAuthorize(Permission.SECURITY_REFERENCE_READ)
 	public @ResponseBody
@@ -119,7 +156,19 @@ public abstract class AbstractAuditableReferenceController<T extends AbstractRef
 		return this.instantiateTO(model);
 	}
 
-	@Override
+	/**
+	 * Persist a new instance of the specified object.
+	 * <p>
+	 * Must not include an id.
+	 * 
+	 * @param obj
+	 *            New instance to persist.
+	 * @return Original instance plus the generated id.
+	 * @throws ObjectNotFoundException
+	 *             If specified object could not be found.
+	 * @throws ValidationException
+	 *             If the specified data contains an id (since it shouldn't).
+	 */
 	@RequestMapping(method = RequestMethod.POST)
 	public @ResponseBody
 	TO create(@Valid @RequestBody final TO obj) throws ObjectNotFoundException,
@@ -140,7 +189,19 @@ public abstract class AbstractAuditableReferenceController<T extends AbstractRef
 		return null;
 	}
 
-	@Override
+	/**
+	 * Persist any changes to the specified instance.
+	 * 
+	 * @param id
+	 *            Explicit id to the instance to persist.
+	 * @param obj
+	 *            Full instance to persist.
+	 * @return The update data object instance.
+	 * @throws ObjectNotFoundException
+	 *             If specified object could not be found.
+	 * @throws ValidationException
+	 *             If the specified id is null.
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public @ResponseBody
 	TO save(@PathVariable final UUID id, @Valid @RequestBody final TO obj)
@@ -164,7 +225,16 @@ public abstract class AbstractAuditableReferenceController<T extends AbstractRef
 		return null;
 	}
 
-	@Override
+	/**
+	 * Marks the specified data instance with a status of
+	 * {@link ObjectStatus#INACTIVE}.
+	 * 
+	 * @param id
+	 *            The id of the data instance to mark deleted.
+	 * @return Success boolean.
+	 * @throws ObjectNotFoundException
+	 *             If specified object could not be found.
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public @ResponseBody
 	ServiceResponse delete(@PathVariable final UUID id)
