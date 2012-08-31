Jasig/SSP

commit ecc8ed5df16b8692d6b5edb3860bcbc2df6facaa

SSP-448. Externalize more db config into properties files. Introduce ssp-config.default.properties so ssp-config.properties in an external config directory no longer strictly required.

src/main/java/org/jasig/ssp/config/BeanProfileApplicationContextInitializer.java
@@ -20,6 +20,8 @@ public class BeanProfileApplicationContextInitializer
 			.getLogger(BeanProfileApplicationContextInitializer.class);
 
 	private static final String CONFIG_FILE_NAME = "ssp-config.properties";
+	private static final String DEFAULT_CONFIG_FILE_NAME =
+			"ssp-config.default.properties";
 	private static final String FILE_SEPARATOR = System
 			.getProperty("file.separator");
 
@@ -30,6 +32,22 @@ public class BeanProfileApplicationContextInitializer
 		final String propertiesFileName = ExternalConfigLoader
 				.getConfigDir() + FILE_SEPARATOR + CONFIG_FILE_NAME;
 
+		boolean anyPropsLoaded = false;
+		final String defaultPropertiesResourcePath =
+				"classpath:"+DEFAULT_CONFIG_FILE_NAME;
+		try {
+			applicationContext
+					.getEnvironment()
+					.getPropertySources()
+					.addLast(new ResourcePropertySource(
+							defaultPropertiesResourcePath));
+			anyPropsLoaded = true;
+			LOGGER.info("Loaded properties file from {} for determining "
+					+ "spring profile.", defaultPropertiesResourcePath);
+		} catch ( Exception e ) {
+			LOGGER.info("Unable to load properties file {} for determining "
+					+ " spring profile.", defaultPropertiesResourcePath, e);
+		}
 		try {
 			applicationContext
 					.getEnvironment()
@@ -37,15 +55,21 @@ public class BeanProfileApplicationContextInitializer
 					.addLast(
 							new ResourcePropertySource(
 									"file:" + propertiesFileName));
-			LOGGER.info("Loaded properties file from " + propertiesFileName
-					+ " for determining spring profile.");
+			anyPropsLoaded = true;
+			LOGGER.info("Loaded properties file from {} for determining "
+					+ "spring profile.", propertiesFileName);
 		} catch (Exception e) {
-			LOGGER.info("Unable to load properties file " + propertiesFileName +
-					" in BeanProfileApplicationContextInitializer, defaulting "
-					+
-					"spring.profile to uportal");
+			LOGGER.info("Unable to load properties file {} for determining "
+					+ " spring profile.", propertiesFileName, e);
+		}
+
+		if ( !(anyPropsLoaded) ) {
+			LOGGER.info("Unable to load any configuration files for "
+					+ "determining Spring profile. Defaulting spring.profile " +
+					 "to uportal");
 			System.setProperty("spring.profiles.active", "uportal");
 		}
+
 	}
 
 }

src/main/resources/ssp-config.default.properties
@@ -1,5 +1,7 @@
-# Use this file, placed in the production config directory, to override
-# defaults set in {webapp-root}/WEB-INF/classes/ssp-config.default.properties
+# This file is the main configuration file for the SSP application. Override
+# values here with a file named ssp-config.properties placed in the
+# production config directory.
+#
 
 system_id=DEV
 
@@ -32,6 +34,7 @@ db_conns_max_active=10
 db_conns_max_idle=5
 db_conns_max_wait=10000
 db_conns_validation_query=select 1
+
 
 #
 # Mail

src/main/webapp/WEB-INF/spring/app-config.xml
@@ -14,8 +14,15 @@
 	<import resource="local/ssp-local.xml" />
 
 	<!-- Properties file configuration of existing beans -->
-	<util:properties id="configProperties"
-		location="file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties" />
+	<bean id="configProperties" class="org.springframework.beans.factory.config.PropertiesFactoryBean">
+		<property name="ignoreResourceNotFound" value="true" />
+		<property name="locations">
+			<list>
+				<value>classpath:ssp-config.default.properties</value>
+				<value>file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties</value>
+			</list>
+		</property>
+	</bean>
 
 	<util:properties id="contextProperties">
 		<prop key="applicationMode">RUN</prop>
@@ -102,10 +109,10 @@
 		<property name="url" value="#{configProperties.db_url}" />
 		<property name="username" value="#{configProperties.db_username}" />
 		<property name="password" value="#{configProperties.db_password}" />
-		<property name="validationQuery" value="select 1" />
-		<property name="maxActive" value="10" />
-		<property name="maxIdle" value="5" />
-		<property name="maxWait" value="10000" />
+		<property name="validationQuery" value="#{configProperties.db_conns_validation_query}" />
+		<property name="maxActive" value="#{configProperties.db_conns_max_active}" />
+		<property name="maxIdle" value="#{configProperties.db_conns_max_idle}" />
+		<property name="maxWait" value="#{configProperties.db_conns_max_wait}" />
 	</bean>
 	
 	<!-- Message source for this context, loaded from localized "messages_xx" files -->

src/main/webapp/WEB-INF/spring/local/ssp-local.xml
@@ -13,8 +13,16 @@
 	http://www.springframework.org/schema/context http://www.springframework.org/schema/context/spring-context-3.1.xsd
 	http://www.springframework.org/schema/util http://www.springframework.org/schema/util/spring-util-3.1.xsd">
 
-	<util:properties id="configProperties"
-		location="file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties" />
+	<!-- Properties file configuration of existing beans -->
+	<bean id="configProperties" class="org.springframework.beans.factory.config.PropertiesFactoryBean">
+		<property name="ignoreResourceNotFound" value="true" />
+		<property name="locations">
+			<list>
+				<value>classpath:ssp-config.default.properties</value>
+				<value>file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties</value>
+			</list>
+		</property>
+	</bean>
 
 	<!-- Mail Server Configuration -->
 	<bean id="javaMailSender" class="org.springframework.mail.javamail.JavaMailSenderImpl">

src/main/webapp/WEB-INF/spring/local/ssp-sec-dev.xml
@@ -18,8 +18,15 @@ It allows ssp to be run outside in absence of uportal.
 		http://www.springframework.org/schema/util/spring-util-3.1.xsd">
 
 	<!-- Properties file configuration of existing beans -->
-	<util:properties id="configProperties"
-		location="file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties" />
+	<beans:bean id="configProperties" class="org.springframework.beans.factory.config.PropertiesFactoryBean">
+		<beans:property name="ignoreResourceNotFound" value="true" />
+		<beans:property name="locations">
+			<beans:list>
+				<beans:value>classpath:ssp-config.default.properties</beans:value>
+				<beans:value>file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties</beans:value>
+			</beans:list>
+		</beans:property>
+	</beans:bean>
 
 	<ldap-server ldif="file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/users.ldif" 
 			root="dc=springframework,dc=org"

src/main/webapp/WEB-INF/spring/local/ssp-sec-local.xml
@@ -16,8 +16,15 @@
 		http://www.springframework.org/schema/util/spring-util-3.1.xsd">
 
 	<!-- Properties file configuration of existing beans -->
-	<util:properties id="configProperties"
-		location="file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties" />
+	<beans:bean id="configProperties" class="org.springframework.beans.factory.config.PropertiesFactoryBean">
+		<beans:property name="ignoreResourceNotFound" value="true" />
+		<beans:property name="locations">
+			<beans:list>
+				<beans:value>classpath:ssp-config.default.properties</beans:value>
+				<beans:value>file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties</beans:value>
+			</beans:list>
+		</beans:property>
+	</beans:bean>
 
 	<beans:import resource="ssp-sec-dev.xml" />
 

src/main/webapp/WEB-INF/spring/mvc-config.xml
@@ -13,8 +13,15 @@
 		http://www.springframework.org/schema/util	 http://www.springframework.org/schema/util/spring-util-3.1.xsd">
 
 	<!-- Properties file configuration of existing beans -->
-	<util:properties id="configProperties"
-		location="file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties" />
+	<bean id="configProperties" class="org.springframework.beans.factory.config.PropertiesFactoryBean">
+		<property name="ignoreResourceNotFound" value="true" />
+		<property name="locations">
+			<list>
+				<value>classpath:ssp-config.default.properties</value>
+				<value>file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties</value>
+			</list>
+		</property>
+	</bean>
 
 	<context:component-scan base-package="org.jasig.ssp.web,org.jasig.mygps.web"/>
 		<!-- context:include-filter type="regex" expression=".*Controller"/>

src/main/webapp/WEB-INF/spring/security-config.xml
@@ -10,8 +10,15 @@
 	<beans:import resource="local/ssp-sec-local.xml"/>
 
 	<!-- Properties file configuration of existing beans -->
-	<util:properties id="configProperties" 
-		location="file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties"/>
+	<beans:bean id="configProperties" class="org.springframework.beans.factory.config.PropertiesFactoryBean">
+		<beans:property name="ignoreResourceNotFound" value="true" />
+		<beans:property name="locations">
+			<beans:list>
+				<beans:value>classpath:ssp-config.default.properties</beans:value>
+				<beans:value>file:#{T(org.jasig.ssp.config.logging.ExternalConfigLoader).getConfigDir()}/ssp-config.properties</beans:value>
+			</beans:list>
+		</beans:property>
+	</beans:bean>
 
 	<global-method-security pre-post-annotations="enabled" />
 

commit 14fc486d5aafee7af9614fc93df1118228501d1c

NOJIRA. Reverse property file load order for selecting Spring profiles. This allows the external config file to override the default. Fixes a regression from ecc8ed5df16.

src/main/java/org/jasig/ssp/config/BeanProfileApplicationContextInitializer.java
@@ -33,6 +33,22 @@ public class BeanProfileApplicationContextInitializer
 				.getConfigDir() + FILE_SEPARATOR + CONFIG_FILE_NAME;
 
 		boolean anyPropsLoaded = false;
+
+		try {
+			applicationContext
+					.getEnvironment()
+					.getPropertySources()
+					.addLast(
+							new ResourcePropertySource(
+									"file:" + propertiesFileName));
+			anyPropsLoaded = true;
+			LOGGER.info("Loaded properties file from {} for determining "
+					+ "spring profile.", propertiesFileName);
+		} catch (Exception e) {
+			LOGGER.info("Unable to load properties file {} for determining "
+					+ " spring profile.", propertiesFileName, e);
+		}
+
 		final String defaultPropertiesResourcePath =
 				"classpath:"+DEFAULT_CONFIG_FILE_NAME;
 		try {
@@ -48,20 +64,7 @@ public class BeanProfileApplicationContextInitializer
 			LOGGER.info("Unable to load properties file {} for determining "
 					+ " spring profile.", defaultPropertiesResourcePath, e);
 		}
-		try {
-			applicationContext
-					.getEnvironment()
-					.getPropertySources()
-					.addLast(
-							new ResourcePropertySource(
-									"file:" + propertiesFileName));
-			anyPropsLoaded = true;
-			LOGGER.info("Loaded properties file from {} for determining "
-					+ "spring profile.", propertiesFileName);
-		} catch (Exception e) {
-			LOGGER.info("Unable to load properties file {} for determining "
-					+ " spring profile.", propertiesFileName, e);
-		}
+
 
 		if ( !(anyPropsLoaded) ) {
 			LOGGER.info("Unable to load any configuration files for "