f5ac41933e61f8e19f45b3ad60691d22958e39aa
Dan McCallum <dan.mccallum@gmail.com>
2012-11-05 23:04:58 -0700

    SSP-565 Allow person search without program status

--- src/main/webapp/app/service/SearchService.js
+++ src/main/webapp/app/service/SearchService.js
@@ -33,37 +33,61 @@ Ext.define('Ssp.service.SearchService', {
     	return baseUrl;
     },
 
-    search: function( searchTerm, outsideCaseload, callbacks ){
-    	var me=this;
+	searchWithParams: function(params, callbacks) {
+		var me=this;
 
-	    me.store.removeAll();
+		me.store.removeAll();
 
 		// Set params in the url for Search Store
-		// because the params need to be applied prior to load and not in a params 
+		// because the params need to be applied prior to load and not in a params
 		// definition from the load method or the paging
 		// toolbar applied to the SearchView will not
 		// apply the params when using next or previous page
-    	Ext.apply(me.store.getProxy(),{url: me.getBaseUrl()+'?sort=lastName&outsideCaseload='+outsideCaseload+'&searchTerm='+searchTerm});
 
-	    me.store.load({
-		    params: {
-		        
-		    },
-		    callback: function(records, operation, success) {
-		        if (success)
-		        {
-			    	if (callbacks != null)
-			    	{
-			    		callbacks.success( records, callbacks.scope );
-			    	}		        	
-		        }else{
-			    	if (callbacks != null)
-			    	{
-			    		callbacks.failure( records, callbacks.scope );
-			    	}
-		        }
-		    },
-		    scope: me
+		queryStr = "";
+		for (var paramName in params) {
+			// TODO url encoding?
+			if ( queryStr ) {
+				queryStr += "&";
+			}
+			queryStr += paramName + "=" + params[paramName];
+		}
+		if ( !("sort" in params) ) {
+			if ( queryStr ) {
+				queryStr += "&";
+			}
+			queryStr += "sort=lastName";
+		}
+
+		Ext.apply(me.store.getProxy(),{url: me.getBaseUrl()+'?'+queryStr});
+
+		me.store.load({
+			params: {
+
+			},
+			callback: function(records, operation, success) {
+				if (success)
+				{
+					if (callbacks != null)
+					{
+						callbacks.success( records, callbacks.scope );
+					}
+				}else{
+					if (callbacks != null)
+					{
+						callbacks.failure( records, callbacks.scope );
+					}
+				}
+			},
+			scope: me
 		});
+	},
+
+    search: function( searchTerm, outsideCaseload, callbacks ){
+    	var me = this;
+		me.searchWithParams({
+			searchTerm: searchTerm,
+			outsideCaseload: outsideCaseload
+		}, callbacks);
     }
 });
