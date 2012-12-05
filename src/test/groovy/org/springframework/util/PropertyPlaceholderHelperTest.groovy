c24a78fc05b491d1a99c08ce9583a6d05f024eed
Dan McCallum <dan.mccallum@gmail.com>
2012-12-05 15:16:31 -0700

    SSP-584 Fix hard-coded db name in Liquibase script 000014.xml.
    To get this to work without duplicating the database name in
    config, I introduced the ability to use property placeholders
    in ssp-config.properties. So, for example, the default db
    config now looks like:
    
      db_name=ssp
      db_url=jdbc:postgresql://127.0.0.1:5432/${db_name}
    
    Also adds the ability to turn off that script by setting this in
    ssp-config.properties:
    
      db_liquibase_set_mssql_snapshot_isolation=false
    
    Felt that was needed b/c I've seen cases in dev where the command
    never completes successfully (either when run from Liquibase or
    the MSSQL management console). Seems to be related to trying to
    run the command while other connections are still open to the db.
    So seemed like it would be useful to let the app start up while
    admins figured out how to apply the change out of band.

---
+++ src/test/groovy/org/springframework/util/PropertyPlaceholderHelperTest.groovy
@@ -0,0 +1,39 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.springframework.util
+
+import spock.lang.Specification
+
+class PropertyPlaceholderHelperTest extends Specification {
+
+	def "test spring PropertyPlaceholderHelper works like we expect"() {
+		when:
+		def props = [ "foo":"bar", "baz":"\${foo}", "bap":"\${baz}"] as Properties
+
+		and:
+		def drefHelper = new PropertyPlaceholderHelper("\${", "}")
+		def drefProps = [:]
+		props.each { k, v ->
+			drefProps[k] = drefHelper.replacePlaceholders(v, props);
+		}
+
+		then:
+		drefProps == [ "foo":"bar", "baz":"bar", "bap":"bar" ]
+	}
+}
