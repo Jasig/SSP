dacb2fb7319fc2057e7f6dade331a7efdc4d7e6e
jstanley70 <jstanley@unicon.net>
2013-02-01 15:29:59 -0700

    SSP-650	Disability Services Report added Disability Report Controller

---
+++ src/main/java/org/jasig/ssp/web/api/reports/DisabilityServicesReportController.java
@@ -0,0 +1,201 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jasig.ssp.web.api.reports; // NOPMD
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jasperreports.engine.JRException;
+import org.jasig.ssp.factory.PersonTOFactory;
+import org.jasig.ssp.model.ObjectStatus;
+import org.jasig.ssp.model.Person;
+import org.jasig.ssp.security.permissions.Permission;
+import org.jasig.ssp.service.ObjectNotFoundException;
+import org.jasig.ssp.service.PersonService;
+import org.jasig.ssp.service.external.TermService;
+import org.jasig.ssp.service.reference.DisabilityStatusService;
+import org.jasig.ssp.service.reference.DisabilityTypeService;
+import org.jasig.ssp.service.reference.ProgramStatusService;
+import org.jasig.ssp.service.reference.ReferralSourceService;
+import org.jasig.ssp.service.reference.SpecialServiceGroupService;
+import org.jasig.ssp.service.reference.StudentTypeService;
+import org.jasig.ssp.transferobject.PersonTO;
+import org.jasig.ssp.transferobject.reports.DisabilityServicesReportTO;
+import org.jasig.ssp.transferobject.reports.PersonSearchFormTO;
+import org.jasig.ssp.util.sort.SortingAndPaging;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.Maps;
+
+/**
+ * Service methods for manipulating data about people in the system.
+ * <p>
+ * Mapped to URI path <code>report/AddressLabels</code>
+ */
+@Controller
+@RequestMapping("/1/report/disabilityservices")
+public class DisabilityServicesReportController extends ReportBaseController { // NOPMD
+
+	private static String REPORT_URL = "/reports/disabilityServices.jasper";
+	private static String REPORT_FILE_TITLE = "Disability_Services_Report";
+	
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(AddressLabelsReportController.class);
+
+	@Autowired
+	private transient PersonService personService;
+	@Autowired
+	private transient PersonTOFactory personTOFactory;
+	@Autowired
+	private transient SpecialServiceGroupService ssgService;
+	@Autowired
+	private transient ReferralSourceService referralSourcesService;
+	@Autowired
+	private transient TermService termService;
+	@Autowired
+	private transient ProgramStatusService programStatusService;
+	@Autowired
+	protected transient StudentTypeService studentTypeService;	
+	@Autowired
+	protected transient DisabilityStatusService disabilityStatusService;	
+	
+	@Autowired
+	protected transient DisabilityTypeService disabilityTypeService;
+
+	// @Autowired
+	// private transient PersonTOFactory factory;
+
+	@InitBinder
+	public void initBinder(final WebDataBinder binder) {
+		final SimpleDateFormat dateFormat = new SimpleDateFormat(DEFAULT_DATE_FORMAT,
+				Locale.US);
+		dateFormat.setLenient(false);
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, true));
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	@PreAuthorize(Permission.SECURITY_REPORT_READ)
+	@ResponseBody
+	public void getDisabilityServicesReport(
+			final HttpServletResponse response,
+			final @RequestParam(required = false) ObjectStatus status,
+			final @RequestParam(required = true) UUID coachId,	
+			final @RequestParam(required = false) UUID odsCoachId,
+			final @RequestParam(required = false) UUID disabilityStatusId,
+			final @RequestParam(required = false) UUID disabilityTypeId,
+			final @RequestParam(required = false) UUID programStatus,
+			final @RequestParam(required = false) List<UUID> specialServiceGroupIds,
+			final @RequestParam(required = false) List<UUID> referralSourcesIds,
+			final @RequestParam(required = false) List<UUID> studentTypeIds,
+			final @RequestParam(required = false) Integer anticipatedStartYear,
+			final @RequestParam(required = false) String anticipatedStartTerm,
+			final @RequestParam(required = false) Integer registrationYear,
+			final @RequestParam(required = false) String registrationTerm,
+			final @RequestParam(required = false) Date createDateFrom,
+			final @RequestParam(required = false) Date createDateTo,
+			final @RequestParam(required = false) String termCode,
+			final @RequestParam(required = false, defaultValue = DEFAULT_REPORT_TYPE) String reportType)
+			throws ObjectNotFoundException, JRException, IOException {
+		
+		final Map<String, Object> parameters = Maps.newHashMap();
+		final PersonSearchFormTO personSearchForm = new PersonSearchFormTO();
+		SearchParameters.addCoach(coachId, parameters, personSearchForm, personService, personTOFactory);
+		SearchParameters.addOdsCoach(odsCoachId, parameters, personSearchForm, personService, personTOFactory);
+		
+		SearchParameters.addReferenceLists(studentTypeIds, 
+				specialServiceGroupIds, 
+				referralSourcesIds, 
+				parameters, 
+				personSearchForm, 
+				studentTypeService, 
+				ssgService, 
+				referralSourcesService);
+		
+		SearchParameters.addDateRange(createDateFrom, 
+				createDateTo, 
+				termCode, 
+				parameters, 
+				personSearchForm, 
+				termService);
+		
+		SearchParameters.addReferenceTypes(programStatus, 
+				disabilityStatusId, 
+				disabilityTypeId,
+				true,
+				parameters, 
+				personSearchForm, 
+				programStatusService, 
+				disabilityStatusService,
+				disabilityTypeService);
+		
+		SearchParameters.addAnticipatedAndRegistrationTerms(anticipatedStartTerm, 
+				anticipatedStartYear, 
+				registrationTerm, 
+				registrationYear, 
+				parameters, 
+				personSearchForm);
+
+		// TODO Specifying person name sort fields in the SaP doesn't seem to
+		// work... end up with empty results need to dig into actual query
+		// building
+		final List<Person> people = personService.peopleFromCriteria(
+				personSearchForm, SortingAndPaging.createForSingleSort(status, null,
+						null, null, null, null));
+		
+		Collections.sort(people, Person.PERSON_NAME_AND_ID_COMPARATOR);
+		List<DisabilityServicesReportTO> reportTOs = new ArrayList<DisabilityServicesReportTO>();
+		
+		for(Person person:people){
+			DisabilityServicesReportTO reportTO = new DisabilityServicesReportTO();
+			reportTO.setPerson(person);
+			reportTOs.add(reportTO);
+		}
+		
+		SearchParameters.addStudentCount(people, parameters);
+		
+		generateReport(response, parameters, reportTOs, REPORT_URL, reportType, REPORT_FILE_TITLE);
+
+	}
+
+	@Override
+	protected Logger getLogger() {
+		return LOGGER;
+	}
+}
