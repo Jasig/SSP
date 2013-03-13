97af248bd80a8d49172a8220987507159425ec99
Archna Jindal <archna.jindal@sinclair.edu>
2013-03-13 08:15:46 -0400

    SSP-677 Save and Cancel button consistency

--- src/main/webapp/all-classes.js
+++ src/main/webapp/all-classes.js
@@ -18518,102 +18518,113 @@ Ext.define('Ssp.view.ProgramStatusChangeReasonWindow', {
  * under the License.
  */
 Ext.define('Ssp.view.person.CaseloadAssignment', {
-	extend: 'Ext.panel.Panel',
-	alias : 'widget.caseloadassignment',
-	mixins: [ 'Deft.mixin.Injectable',
-              'Deft.mixin.Controllable'],
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.caseloadassignment',
+    mixins: ['Deft.mixin.Injectable', 'Deft.mixin.Controllable'],
     controller: 'Ssp.controller.person.CaseloadAssignmentViewController',
     inject: {
-    	model: 'currentPerson',
-    	sspConfig: 'sspConfig'
+        model: 'currentPerson',
+        sspConfig: 'sspConfig'
     },
     width: '100%',
-	height: '100%',   
-	initComponent: function() {
-		var me=this;
-		Ext.apply(me, 
-				{
-			        title: "Caseload Assignment",
-		    		autoScroll: true,
-		    	    defaults: {
-		    	        bodyStyle: 'padding:5px'
-		    	    },
-		    	    layout: {
-		    	        type: 'accordion',
-		    	        align: 'stretch',
-		    	        titleCollapse: true,
-		    	        animate: true,
-		    	        activeOnTop: true
-		    	    },		    		
-		    		dockedItems: [{
-  		               xtype: 'toolbar',
-  		               dock: 'top',
-  		               items: [{
-  		                         xtype: 'label',
-  		                         text: 'Fill out the following forms with assigned coach details and appointment information'
-  		                       }]  
-  		            },{
-				        dock: 'bottom',
-				        xtype: 'toolbar',
-				        items: [{
-			                     xtype: 'button', 
-				        	     itemId: 'saveButton', 
-				        	     text:'Save'
-				        	    },
-				                 {
-				            	   xtype: 'button',
-				            	   itemId: 'cancelButton',
-				            	   text: 'Cancel',
-				                 }/*,{
-							        xtype: 'checkbox',
-							        boxLabel: 'Send Student Intake Request', 
-							        name: 'sendStudentIntakeRequest'
-								 },{ 
-						        	xtype: 'tbspacer',
-						        	width: 25
-						         },{
-							    	xtype: 'displayfield',
-							        fieldLabel: 'Last Request Date',
-							        name: 'lastStudentIntakeRequestDate',
-							        value: ((me.model.getFormattedStudentIntakeRequestDate().length > 0) ? me.person.getFormattedStudentIntakeRequestDate() : 'No requests have been sent')
-								 }*/,{ 
-						        	xtype: 'tbspacer',
-						        	flex: 1
-						         },{
-					        	     xtype: 'checkboxfield',
-				                     boxLabel: "Reset the student to Active status on the assigned " + me.sspConfig.get('coachFieldLabel') + "'s Caseload",
-				                     itemId: 'resetActiveStatusCheck',
-				                     name: 'setActiveStatus',
-				                     //hidden: (( me.model.get('currentProgramStatusName').toLowerCase() == 'active' && me.model.get('id') != "")? true : false),
-				                     inputValue: false
-				                  },
-				                 {
-				            	   xtype: 'button',
-				            	   itemId: 'printButton',
-				            	   tooltip: 'Print Appointment Form',
-				            	   hidden: true,
-				            	   width: 30,
-						           height: 30,
-						           cls: 'printIcon'
-				                 },
-				                 {
-				            	   xtype: 'button',
-				            	   itemId: 'emailButton',
-				            	   hidden: true,
-				            	   tooltip: 'Email Appointment Form',
-				            	   width: 30,
-						           height: 30,
-						           cls: 'emailIcon'
-				                 }]
-				    }],
-				    
-				    items: [ ]
-			});
-	
-		return me.callParent(arguments);
-	}
-
+    height: '100%',
+    initComponent: function(){
+        var me = this;
+        Ext.apply(me, {
+            title: "Caseload Assignment",
+            autoScroll: true,
+            defaults: {
+                bodyStyle: 'padding:5px'
+            },
+            layout: {
+                type: 'accordion',
+                align: 'stretch',
+                titleCollapse: true,
+                animate: true,
+                activeOnTop: true
+            },
+            dockedItems: [{
+                xtype: 'toolbar',
+                dock: 'top',
+                items: [{
+                    xtype: 'container',
+                    layout: 'vbox',
+                    items: [{
+                        xtype: 'label',
+                        text: 'Fill out the following forms with assigned coach details and appointment information'
+                    }, {
+                        xtype: 'tbspacer',
+                        height: '10'
+                    }, {
+                        xtype: 'container',
+                        layout: 'hbox',
+                        items: [{
+                            xtype: 'button',
+                            itemId: 'saveButton',
+                            text: 'Save'
+                        }, {
+                            xtype: 'tbspacer',
+                            height: '20'
+                        }, {
+                            xtype: 'button',
+                            itemId: 'cancelButton',
+                            text: 'Cancel',
+                        }]
+                    }]
+                }]
+            }, {
+                dock: 'bottom',
+                xtype: 'toolbar',
+                items: [                /*,{
+                 xtype: 'checkbox',
+                 boxLabel: 'Send Student Intake Request',
+                 name: 'sendStudentIntakeRequest'
+                 },{
+                 xtype: 'tbspacer',
+                 width: 25
+                 },{
+                 xtype: 'displayfield',
+                 fieldLabel: 'Last Request Date',
+                 name: 'lastStudentIntakeRequestDate',
+                 value: ((me.model.getFormattedStudentIntakeRequestDate().length > 0) ? me.person.getFormattedStudentIntakeRequestDate() : 'No requests have been sent')
+                 }*/
+                , {
+                    xtype: 'tbspacer',
+                    flex: 1
+                }, {
+                    xtype: 'checkboxfield',
+                    boxLabel: "Reset the student to Active status on the assigned " + me.sspConfig.get('coachFieldLabel') + "'s Caseload",
+                    itemId: 'resetActiveStatusCheck',
+                    name: 'setActiveStatus',
+                    //hidden: (( me.model.get('currentProgramStatusName').toLowerCase() == 'active' && me.model.get('id') != "")? true : false),
+                    inputValue: false
+                }, {
+                    xtype: 'button',
+                    itemId: 'printButton',
+                    tooltip: 'Print Appointment Form',
+                    hidden: true,
+                    width: 30,
+                    height: 30,
+                    cls: 'printIcon'
+                }, {
+                    xtype: 'button',
+                    itemId: 'emailButton',
+                    hidden: true,
+                    tooltip: 'Email Appointment Form',
+                    width: 30,
+                    height: 30,
+                    cls: 'emailIcon'
+                }]
+            }],
+            
+            items: []
+        });
+        
+        return me.callParent(arguments);
+    }
+    
 });
+
 /*
  * Licensed to Jasig under one or more contributor license
  * agreements. See the NOTICE file distributed with this work
