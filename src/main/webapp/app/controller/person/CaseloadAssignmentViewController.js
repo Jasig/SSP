a225723871a6201ebf5bc0abc13379402200f726
aarland <aarland@unicon.net>
2013-03-14 17:04:53 -0600

    SSP-839 checking intake request before submitting appointment form

--- src/main/webapp/app/controller/person/CaseloadAssignmentViewController.js
+++ src/main/webapp/app/controller/person/CaseloadAssignmentViewController.js
@@ -463,7 +463,7 @@ Ext.define('Ssp.controller.person.CaseloadAssignmentViewController', {
     saveAppointment: function(){
     	var me=this;
     	var jsonData, personId;
-    	if (me.appointment.get('appointmentDate') != null && me.appointment.get('startTime') != null && me.appointment.get('endTime') !=null)
+    	if (me.appointment.get('appointmentDate') != null && me.appointment.get('startTime') != null && me.appointment.get('endTime') !=null && me.appointment.get('studentIntakeRequested'))
 		{
     		// Fix dates for GMT offset to UTC
     		me.currentPersonAppointment.set( 'startTime', me.formUtils.fixDateOffsetWithTime(me.appointment.getStartDate() ) );
