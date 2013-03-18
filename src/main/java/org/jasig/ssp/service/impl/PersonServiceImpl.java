ddea6973dcc39f0a64068362818de0e291577d4e
jstanley70 <jstanley@unicon.net>
2013-03-18 15:28:46 -0700

    NOJIRA added mock and set for earlyAlertService for tests.

--- src/main/java/org/jasig/ssp/service/impl/PersonServiceImpl.java
+++ src/main/java/org/jasig/ssp/service/impl/PersonServiceImpl.java
@@ -444,6 +444,11 @@ public class PersonServiceImpl implements PersonService {
 		this.registrationStatusByTermService = registrationStatusByTermService;
 	}
 
+	protected void setEarlyAlertService(
+			final EarlyAlertService earlyAlertService) {
+		this.earlyAlertService = earlyAlertService;
+	}
+
 	@Override
 	public List<Person> peopleFromListOfIds(final List<UUID> personIds,
 			final SortingAndPaging sAndP) {
