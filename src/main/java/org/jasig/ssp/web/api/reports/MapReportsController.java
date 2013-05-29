3a9c955ffef5b5986a962edcdc09ad8ca88e7847
jstanley70 <jstanley@unicon.net>
2013-05-29 08:40:40 -0700

    NOJIRA Added license agreement

--- src/main/java/org/jasig/ssp/web/api/reports/MapReportsController.java
+++ src/main/java/org/jasig/ssp/web/api/reports/MapReportsController.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jasig.ssp.web.api.reports;
 
 import java.io.IOException;
@@ -20,11 +38,17 @@ import org.jasig.ssp.service.EarlyAlertResponseService;
 import org.jasig.ssp.service.EarlyAlertService;
 import org.jasig.ssp.service.ObjectNotFoundException;
 import org.jasig.ssp.service.PersonService;
+import org.jasig.ssp.service.PlanService;
 import org.jasig.ssp.service.external.ExternalPersonService;
 import org.jasig.ssp.service.external.RegistrationStatusByTermService;
 import org.jasig.ssp.service.external.TermService;
 import org.jasig.ssp.service.reference.CampusService;
 import org.jasig.ssp.transferobject.reports.EarlyAlertTermCaseCountsTO;
+import org.jasig.ssp.transferobject.reports.PlanAdvisorCountTO;
+import org.jasig.ssp.transferobject.reports.PlanCourseCountTO;
+import org.jasig.ssp.transferobject.reports.PlanStudentStatusTO;
+import org.jasig.ssp.transferobject.reports.SearchPlanTO;
+import org.jasig.ssp.util.DateTerm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,7 +83,9 @@ public class MapReportsController extends ReportBaseController {
 	@Autowired
 	protected transient TermService termService;
 	
-
+	@Autowired
+	protected transient PlanService planService;
+	
 	
 	@InitBinder
 	public void initBinder(final WebDataBinder binder) {
@@ -84,10 +110,14 @@ public class MapReportsController extends ReportBaseController {
 		
 	
 		List<Term> terms = SearchParameters.getTerms(Lists.newArrayList(termCode), termService);
+		SearchPlanTO form = new SearchPlanTO(null, subjectAbbreviation, courseNumber, terms, null, null);
+		 List<PlanCourseCountTO> counts = planService.getPlanCourseCount(form);
+		 
 		final Map<String, Object> parameters = Maps.newHashMap();
-		
 		SearchParameters.addTermsToMap( terms, parameters);
-		generateReport( response,  parameters, null,  REPORT_URL_NUMBER_COURSES_IN_PLAN, 
+		SearchParameters.addPlanSearchForm(form, parameters);
+		
+		generateReport( response,  parameters, counts,  REPORT_URL_NUMBER_COURSES_IN_PLAN, 
 				 reportType, REPORT_FILE_TITLE_NUMBER_COURSES_IN_PLAN);
 	}
 	
@@ -102,14 +132,16 @@ public class MapReportsController extends ReportBaseController {
 			final @RequestParam(required = false, defaultValue = DEFAULT_REPORT_TYPE) String reportType)
 			throws ObjectNotFoundException, JRException, IOException {
 		
-	
-				
-		List<Term> terms = SearchParameters.getTerms(Lists.newArrayList(termCode), termService);
+		DateTerm dateTerm = new DateTerm(createDateFrom, createDateTo, termCode, termService);
+		
+		SearchPlanTO form = new SearchPlanTO(null, null, null, Lists.newArrayList(dateTerm.getTerm()), createDateFrom, createDateTo);
+		List<PlanAdvisorCountTO> counts = planService.getAdvisorsPlanCount(form);
+		
 		final Map<String, Object> parameters = Maps.newHashMap();
+		SearchParameters.addPlanSearchForm(form, parameters);
+		SearchParameters.addDateTermToMap(dateTerm, parameters);
 		
-		SearchParameters.addTermsToMap( terms, parameters);
-		
-		generateReport( response,  parameters, null,  REPORT_URL_NUMBER_PLANS_BY_ADVISOR, 
+		generateReport( response,  parameters, counts,  REPORT_URL_NUMBER_PLANS_BY_ADVISOR, 
 				 reportType, REPORT_FILE_TITLE_NUMBER_PLANS_BY_ADVISOR);
 	}
 	
@@ -125,12 +157,18 @@ public class MapReportsController extends ReportBaseController {
 			final @RequestParam(required = false, defaultValue = DEFAULT_REPORT_TYPE) String reportType)
 			throws ObjectNotFoundException, JRException, IOException {
 		
+		
 	
 		List<Term> terms = SearchParameters.getTerms(Lists.newArrayList(termCode), termService);
+		
+		SearchPlanTO form = new SearchPlanTO(planStatus, subjectAbbreviation, courseNumber, terms, null, null);
+		 List<PlanStudentStatusTO> counts = planService.getPlanStudentStatusByCourse(form);
+			
+		
 		final Map<String, Object> parameters = Maps.newHashMap();
 		
 		SearchParameters.addTermsToMap(terms , parameters);
-		generateReport( response,  parameters, null,  REPORT_URL_NUMBER_STUDENTS_BY_STATUS, 
+		generateReport( response,  parameters, counts,  REPORT_URL_NUMBER_STUDENTS_BY_STATUS, 
 				 reportType, REPORT_FILE_TITLE_NUMBER_STUDENTS_BY_STATUS);
 	}
 
