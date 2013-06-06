12a9d444fbcd26cea6a36920d9d83b27cc0f8328
jstanley70 <jstanley@unicon.net>
2013-06-06 12:51:02 -0700

    SSP-1327 loading mask added Would be nice to see how performance degrades with significant templatees currently bringing back quite a bit of data.

--- src/main/webapp/app/controller/tool/map/LoadTemplateViewController.js
+++ src/main/webapp/app/controller/tool/map/LoadTemplateViewController.js
@@ -103,17 +103,24 @@ Ext.define('Ssp.controller.tool.map.LoadTemplateViewController', {
 	init: function() {
 		var me=this;
 	    me.resetForm();
+	    me.store.addListener("load", me.onStoreLoaded, me);
+	    me.getView().setLoading(true);
 	    me.store.load();
 		//TODO this should be automatic
 
 		return me.callParent(arguments);
     },
+    
+    onStoreLoaded: function(){
+    	var me = this;
+    	me.getView().setLoading(false);
+    },
 
-	
     resetForm: function() {
         var me = this;
          me.getView().query("form")[0].getForm().reset();
     },
+    
     onOpenClick: function(button) {
     	var me = this;
 		var grid, record;
@@ -169,93 +176,92 @@ Ext.define('Ssp.controller.tool.map.LoadTemplateViewController', {
 	        me.handleSelect(me);
 	        var params = {};
 	        me.setParam(params, me.getProgram(), "programCode");
-	    },  
-	    
-	    onProgramCancelClick: function(button){
-	        var me=this;
-	        me.getProgram().setValue("");
-	        me.handleSelect(me);
-	    },
-	    
-	    onDepartmentSelect: function(){
-	        var me=this;
-			me.handleSelect(me);
-	    }, 
-	    
-	    onDepartmentCancelClick: function(button){
-	        var me=this;
-	        me.getDepartment().setValue("");
-	        me.handleSelect(me);
-	    },
-	    
-	    onDivisionSelect: function(){
-	        var me=this;
-			me.handleSelect(me);
-	    },   
-	    
-	    onDivisionCancelClick: function(button){
-	        var me=this;
-	        me.getDivision().setValue("");
-	        me.handleSelect(me);
-	    },
-	    
-	    handleSelect: function(me){
-	    	var params = {};
-	    	me.setParam(params, me.getProgram(), 'programCode');
-	    	me.setParam(params, me.getDepartment(), 'departmentCode');
-	    	me.setParam(params, me.getDivision(), 'divisionCode');
-			params["objectStatus"] = "ALL"; //Object status and object type filtered client side.
-	    	me.store.on('load', me.onLoadComplete, this, {single: true});
-	    	me.store.load({params: params});
-	    },
-	    
-	    onLoadComplete: function(){
-			var me = this;
-	    	me.onObjectStatusFilterSelect();
-	    },
-	    
-	    setParam: function(params, field, fieldName){
-	    	if(field.getValue() && field.getValue().length > 0)
-	    		params[fieldName] = field.getValue();
-	    },
+    },  
+    
+    onProgramCancelClick: function(button){
+        var me=this;
+        me.getProgram().setValue("");
+        me.handleSelect(me);
+    },
+    
+    onDepartmentSelect: function(){
+        var me=this;
+		me.handleSelect(me);
+    }, 
+    
+    onDepartmentCancelClick: function(button){
+        var me=this;
+        me.getDepartment().setValue("");
+        me.handleSelect(me);
+    },
+    
+    onDivisionSelect: function(){
+        var me=this;
+		me.handleSelect(me);
+    },   
+    
+    onDivisionCancelClick: function(button){
+        var me=this;
+        me.getDivision().setValue("");
+        me.handleSelect(me);
+    },
+    
+    handleSelect: function(me){
+    	var params = {};
+    	me.setParam(params, me.getProgram(), 'programCode');
+    	me.setParam(params, me.getDepartment(), 'departmentCode');
+    	me.setParam(params, me.getDivision(), 'divisionCode');
+		params["objectStatus"] = "ALL"; //Object status and object type filtered client side.
+    	me.store.on('load', me.onLoadComplete, this, {single: true});
+    	me.store.load({params: params});
+    },
+    
+    onLoadComplete: function(){
+		var me = this;
+    	me.onObjectStatusFilterSelect();
+    },
+    
+    setParam: function(params, field, fieldName){
+    	if(field.getValue() && field.getValue().length > 0)
+    		params[fieldName] = field.getValue();
+    },
+
+	onTypeFilterSelect:function(){
+		var me = this;
+		me.onObjectStatusFilterSelect();
+	},
 	
-		onTypeFilterSelect:function(){
+	onObjectStatusFilterSelect:function(){
 			var me = this;
-			me.onObjectStatusFilterSelect();
-		},
-		
-		onObjectStatusFilterSelect:function(){
-				var me = this;
-				me.values = {}
-				me.values.objectStatus = me.getObjectStatusFilter().getValue();
-				me.values.typeValue = me.getTypeFilter().getValue();
-				me.store.clearFilter(false);
-				me.store.filterBy(me.typeStatusFilter, me);
-				
-		},
-		
-		typeStatusFilter: function(record){
-			var me = this;
-			if(me.values.objectStatus != null && me.values.objectStatus != undefined && me.values.objectStatus != 'ALL')
-			{
-				if(record.get("objectStatus") != me.values.objectStatus)
-					return false;
-			}
-			if(me.values.typeValue != null && me.values.typeValue != undefined && me.values.typeValue != 'ALL')
-			{
-				if(record.get("isPrivate") == true && me.values.typeValue == "PUBLIC")
-					return false;
-				if(record.get("isPrivate") == false && me.values.typeValue == "PRIVATE")
-					return false;
-			}
-			return true;
-		},
-	    
-		
-		destroy:function(){
-		    var me=this;
-		    me.store.clearFilter(false);
-		    return me.callParent( arguments );
+			me.values = {}
+			me.values.objectStatus = me.getObjectStatusFilter().getValue();
+			me.values.typeValue = me.getTypeFilter().getValue();
+			me.store.clearFilter(false);
+			me.store.filterBy(me.typeStatusFilter, me);
+	},
+	
+	typeStatusFilter: function(record){
+		var me = this;
+		if(me.values.objectStatus != null && me.values.objectStatus != undefined && me.values.objectStatus != 'ALL')
+		{
+			if(record.get("objectStatus") != me.values.objectStatus)
+				return false;
 		}
+		if(me.values.typeValue != null && me.values.typeValue != undefined && me.values.typeValue != 'ALL')
+		{
+			if(record.get("isPrivate") == true && me.values.typeValue == "PUBLIC")
+				return false;
+			if(record.get("isPrivate") == false && me.values.typeValue == "PRIVATE")
+				return false;
+		}
+		return true;
+	},
+    
+	
+	destroy:function(){
+	    var me=this;
+	    me.store.clearFilter(false);
+	    return me.callParent( arguments );
+	}
 		
 });
