e816c73e60d9b847c5dc2ae7724663116beacbe0
archna.jindal <archna.jindal@sinclair.edu>
2013-07-16 16:52:26 -0400

    SSP - 1420 Cleanup retrieval and rendering of associated/selectable records - Accomodation

--- src/main/java/org/jasig/ssp/web/api/tool/AccommodationController.java
+++ src/main/java/org/jasig/ssp/web/api/tool/AccommodationController.java
@@ -18,9 +18,11 @@
  */
 package org.jasig.ssp.web.api.tool; // NOPMD
 
+import java.util.Collection; 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.List; 
 
 import javax.validation.Valid;
 
@@ -33,7 +35,12 @@ import org.jasig.ssp.service.reference.DisabilityAccommodationService;
 import org.jasig.ssp.service.reference.DisabilityStatusService;
 import org.jasig.ssp.service.reference.DisabilityTypeService;
 import org.jasig.ssp.service.tool.AccommodationService;
+import org.jasig.ssp.transferobject.AbstractAuditableTO; 
+import org.jasig.ssp.transferobject.PersonDisabilityAccommodationTO;
+import org.jasig.ssp.transferobject.PersonDisabilityAgencyTO;
+import org.jasig.ssp.transferobject.PersonDisabilityTypeTO;
 import org.jasig.ssp.transferobject.ServiceResponse;
+import org.jasig.ssp.transferobject.reference.AbstractReferenceTO; 
 import org.jasig.ssp.transferobject.reference.DisabilityAgencyTO;
 import org.jasig.ssp.transferobject.reference.DisabilityAccommodationTO;
 import org.jasig.ssp.transferobject.reference.DisabilityStatusTO;
@@ -52,6 +59,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.Lists; 
 
 /**
  * Accommodation tool services
@@ -125,7 +134,7 @@ public class AccommodationController extends AbstractBaseController {
 			throws ObjectNotFoundException {
 		final AccommodationFormTO formTO = new AccommodationFormTO(
 				service.loadForPerson(studentId));
-		formTO.setReferenceData(referenceData());
+		formTO.setReferenceData(referenceData(formTO)); 
 		return formTO;
 	}
 
@@ -134,10 +143,10 @@ public class AccommodationController extends AbstractBaseController {
 	 * 
 	 * @return Service response with success value, in the JSON format.
 	 */
-	public Map<String, Object> referenceData() {
+	public Map<String, Object> referenceData(AccommodationFormTO formTO) throws ObjectNotFoundException {
 		final Map<String, Object> refData = new HashMap<String, Object>();
 
-		final SortingAndPaging sAndP = new SortingAndPaging(ObjectStatus.ACTIVE);
+		/*final SortingAndPaging sAndP = new SortingAndPaging(ObjectStatus.ACTIVE);
 
 		refData.put("disabilityAccommodations", DisabilityAccommodationTO
 				.toTOList(disabilityAccommodationService.getAll(sAndP).getRows()));		
@@ -149,12 +158,112 @@ public class AccommodationController extends AbstractBaseController {
 				.toTOList(disabilityStatusService.getAll(sAndP).getRows()));		
 		
 		refData.put("disabilityTypes", DisabilityTypeTO
-				.toTOList(disabilityTypeService.getAll(sAndP).getRows()));
+				.toTOList(disabilityTypeService.getAll(sAndP).getRows()));*/
 		
+		
+		final SortingAndPaging sAndP = SortingAndPaging
+				      .createForSingleSortWithPaging(ObjectStatus.ALL, 0, -1, null, null, null);
+				
+				   refData.put("disabilityAccommodations", disabilityAccommodationReferenceDataFor(formTO, sAndP));
+				   refData.put("disabilityAgencies", disabilityAgencieReferenceDataFor(formTO, sAndP));
+				   refData.put("disabilityStatuses", disabilityStatuseReferenceDataFor(formTO, sAndP));
+				   refData.put("disabilityTypes", disabilityTypeReferenceDataFor(formTO, sAndP));
+				
 		
 		return refData;
 	}
 
+	private List<DisabilityAccommodationTO> disabilityAccommodationReferenceDataFor(AccommodationFormTO formTO,
+			                          SortingAndPaging sAndP) {
+			    return filterInactiveExceptFor(
+			       associatedIds(formTO.getPersonDisabilityAccommodations(), PERSON_ASSOC_DISABILITY_ACCOMMODATION_UUID_EXTRACTOR),
+			       DisabilityAccommodationTO.toTOList(disabilityAccommodationService.getAll(sAndP).getRows()));
+			  } 
+	
+	private List<DisabilityAgencyTO> disabilityAgencieReferenceDataFor(AccommodationFormTO formTO,
+            SortingAndPaging sAndP) {
+				return filterInactiveExceptFor(
+				associatedIds(formTO.getPersonDisabilityAgencies(), PERSON_ASSOC_DISABILITY_AGENCY_UUID_EXTRACTOR),
+				DisabilityAgencyTO.toTOList(disabilityAgencyService.getAll(sAndP).getRows()));
+	} 
+	
+	private List<DisabilityTypeTO> disabilityTypeReferenceDataFor(AccommodationFormTO formTO,
+            SortingAndPaging sAndP) {
+				return filterInactiveExceptFor(
+				associatedIds(formTO.getPersonDisabilityTypes(), PERSON_ASSOC_DISABILITY_TYPE_UUID_EXTRACTOR),
+				DisabilityTypeTO.toTOList(disabilityTypeService.getAll(sAndP).getRows()));
+	} 
+	
+	private List<DisabilityStatusTO> disabilityStatuseReferenceDataFor(AccommodationFormTO formTO, SortingAndPaging sAndP) {
+		   final List<DisabilityStatusTO> allTOs =
+				   DisabilityStatusTO.toTOList(disabilityStatusService.getAll(sAndP).getRows());
+	
+		  if ( formTO.getPersonDisability() == null ||
+		      formTO.getPersonDisability().getDisabilityStatusId() == null ) {
+		
+		      return filterInactiveExceptFor(Lists.<UUID>newArrayListWithCapacity(0), allTOs);
+		
+		   }
+		
+		    return filterInactiveExceptFor(
+		       Lists.newArrayList(formTO.getPersonDisability().getDisabilityStatusId()),
+		        allTOs);
+		  } 
+	
+	
+	private <T extends AbstractReferenceTO> List<T>
+	 filterInactiveExceptFor(Collection<UUID> ids, List<T> toFilter) {
+	  List<T> filtered = Lists.newArrayListWithCapacity(toFilter.size());
+	  for ( T filterable : toFilter ) {
+	   if ( filterable.getObjectStatus() == ObjectStatus.ACTIVE ||
+	      ids.contains(filterable.getId())) {
+	     filtered.add(filterable);
+	   	}
+	  }
+	   return filtered;
+	 } 
+	
+	private static interface UUIDExtractor<T extends AbstractAuditableTO> {
+		   UUID fromTO(T to);
+		 } 
+			
+	private static final UUIDExtractor<PersonDisabilityAccommodationTO> PERSON_ASSOC_DISABILITY_ACCOMMODATION_UUID_EXTRACTOR =
+			   new UUIDExtractor<PersonDisabilityAccommodationTO>() {
+			     @Override
+			     public UUID fromTO(PersonDisabilityAccommodationTO to) {
+			       return to.getDisabilityAccommodationId();
+			      }
+			    };
+			    
+    private static final UUIDExtractor<PersonDisabilityAgencyTO> PERSON_ASSOC_DISABILITY_AGENCY_UUID_EXTRACTOR =
+			   new UUIDExtractor<PersonDisabilityAgencyTO>() {
+			     @Override
+			     public UUID fromTO(PersonDisabilityAgencyTO to) {
+			       return to.getDisabilityAgencyId();
+			      }
+			    };
+			    
+    private static final UUIDExtractor<PersonDisabilityTypeTO> PERSON_ASSOC_DISABILITY_TYPE_UUID_EXTRACTOR =
+			   new UUIDExtractor<PersonDisabilityTypeTO>() {
+			     @Override
+			     public UUID fromTO(PersonDisabilityTypeTO to) {
+			       return to.getDisabilityTypeId();
+			      }
+			    };
+
+	// no personassoc superclass for these Person*TOs, hence the callback
+	 private <T extends AbstractAuditableTO> Collection<UUID>
+	 associatedIds(List<T> tos, UUIDExtractor<T> uuidExtractor) {
+	   List<UUID> uuids = Lists.newArrayList();
+	  if ( tos == null ) {
+	    return uuids;
+	  }
+	  for ( T to : tos ) {
+	    uuids.add(uuidExtractor.fromTO(to));
+	  }
+	 return uuids;
+	 } 
+			
 	@Override
 	protected Logger getLogger() {
 		return LOGGER;
