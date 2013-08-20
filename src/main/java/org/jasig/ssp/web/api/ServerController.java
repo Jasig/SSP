cfa7680592c1cdb1a4562b8ac02ad2a130ac5fb8
pspaude <pspaude@unicon.net>
2013-08-20 16:07:12 -0700

    SSP-531 Add API for SSP version/date information

--- src/main/java/org/jasig/ssp/web/api/ServerController.java
+++ src/main/java/org/jasig/ssp/web/api/ServerController.java
@@ -18,16 +18,20 @@
  */
 package org.jasig.ssp.web.api;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TimeZone;
+import java.util.Properties;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.jasig.ssp.transferobject.jsonserializer.DateOnlyFormatting;
 import org.jasig.ssp.util.DateTimeUtils;
 import org.jasig.ssp.util.security.DynamicPermissionChecking;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,6 +40,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/1/server")
 public class ServerController extends AbstractBaseController {
+
+	@Autowired
+	private ServletContext servletContext;
 
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(ServerController.class);
@@ -50,10 +57,35 @@ public class ServerController extends AbstractBaseController {
 		profile.put("timestamp", now.getTime());
 		return profile;
 	}
+	
+	@RequestMapping(value = "/version", method = RequestMethod.GET)
+	@DynamicPermissionChecking
+	public @ResponseBody
+	Map<String,Object> getVersionProfile(HttpServletRequest  request) {
+						
+	    Properties prop = new Properties();
+	    String properties[] = new String[3];
+	    
+	    try {
+	        prop.load(servletContext.getResourceAsStream("/META-INF/MANIFEST.MF"));
+	        properties[0] = prop.getProperty("SSP-Artifact-Version");
+	        properties[1] = prop.getProperty("SSP-Build-Date");
+	        properties[2] = prop.getProperty("SSP-SCM-Revision");
+	    } catch (IOException e) {
+	        LOGGER.error("Error retrieving MANIFEST version information ", e);
+	    }
+				
+		final Map<String,Object> profile = new HashMap<String,Object>();
+		profile.put("artifactVersion", properties[0]);
+		profile.put("buildDate", properties[1]);
+		profile.put("scmRevision", properties[2]);
+		
+		return profile;
+	}
 
 
 	@Override
 	protected Logger getLogger() {
 		return LOGGER;
-	}
+	}	
 }
