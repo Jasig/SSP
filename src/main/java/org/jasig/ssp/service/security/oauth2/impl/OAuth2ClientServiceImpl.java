7c2babc448e830386406b12b847722c7733e81fe
Dan McCallum <dan.mccallum@gmail.com>
2013-08-19 21:01:43 -0700

    SSP-1387 Invalidate Client-only OAuth2 tokens on critical edits
    Still need to address Client soft-deletes.

--- src/main/java/org/jasig/ssp/service/security/oauth2/impl/OAuth2ClientServiceImpl.java
+++ src/main/java/org/jasig/ssp/service/security/oauth2/impl/OAuth2ClientServiceImpl.java
@@ -1,34 +1,32 @@
 package org.jasig.ssp.service.security.oauth2.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.UUID;
-
-import javax.validation.ConstraintViolationException;
-import javax.validation.constraints.NotNull;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.jasig.ssp.dao.AuditableCrudDao;
 import org.jasig.ssp.dao.security.oauth2.OAuth2ClientDao;
 import org.jasig.ssp.factory.OAuth2ClientTOFactory;
+import org.jasig.ssp.model.security.PersistentGrantedAuthority;
 import org.jasig.ssp.model.security.oauth2.OAuth2Client;
 import org.jasig.ssp.service.AbstractAuditableCrudService;
-import org.jasig.ssp.service.AuditableCrudService;
 import org.jasig.ssp.service.ObjectNotFoundException;
 import org.jasig.ssp.service.security.oauth2.OAuth2ClientService;
 import org.jasig.ssp.transferobject.OAuth2ClientTO;
-import org.jasig.ssp.util.sort.PagingWrapper;
-import org.jasig.ssp.util.sort.SortingAndPaging;
 import org.jasig.ssp.web.api.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.BaseClientDetails;
-import org.springframework.security.oauth2.provider.ClientAlreadyExistsException;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.ClientRegistrationException;
-import org.springframework.security.oauth2.provider.ClientRegistrationService;
-import org.springframework.security.oauth2.provider.NoSuchClientException;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,6 +43,12 @@ public class OAuth2ClientServiceImpl extends AbstractAuditableCrudService<OAuth2
 
 	@Autowired
 	private PasswordEncoder passwordEncoder = NoOpPasswordEncoder.getInstance();
+
+	@Autowired
+	private ConsumerTokenServices consumerTokenService;
+
+	@Autowired
+	private ResourceServerTokenServices resourceServerTokenService;
 
 	@Override
 	protected AuditableCrudDao<OAuth2Client> getDao() {
@@ -90,24 +94,6 @@ public class OAuth2ClientServiceImpl extends AbstractAuditableCrudService<OAuth2
 		throw new UnsupportedOperationException("Use save(OAuth2ClientTO)");
 	}
 
-	private void beforeWriteOf(OAuth2Client client) {
-		encodeSecret(client);
-		ensureSchoolId(client);
-	}
-
-	private void encodeSecret(OAuth2Client client) {
-		client.setSecret(StringUtils.trimToNull(client.getSecret()));
-		if ( client.getSecret() != null ) {
-			client.setSecret(passwordEncoder.encode(client.getSecret()));
-		}
-	}
-
-	private void ensureSchoolId(OAuth2Client client) {
-		if (StringUtils.isBlank(client.getSchoolId()) ) {
-			client.setSchoolId(client.getClientId());
-		}
-	}
-
 	@Override
 	public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
 		OAuth2Client client = ((OAuth2ClientDao)getDao()).findByClientId(clientId);
@@ -137,14 +123,76 @@ public class OAuth2ClientServiceImpl extends AbstractAuditableCrudService<OAuth2
 	public OAuth2Client create(OAuth2ClientTO obj) throws
 			ObjectNotFoundException, ValidationException {
 		final OAuth2Client model = factory.from(obj);
-		beforeWriteOf(model);
+		beforeWrite(model, obj);
 		return super.create(model);
 	}
 
 	@Override
 	public OAuth2Client save(OAuth2ClientTO obj) throws ObjectNotFoundException {
-		final OAuth2Client model = factory.from(obj);
-		beforeWriteOf(model);
-		return getDao().save(model);
+		OAuth2Client existingModel = oAuth2ClientDao.get(obj.getId());
+		OAuth2Client newModel = null;
+		boolean invalidateTokens = false;
+		if ( existingModel != null ) {
+			if ( obj.isSecretChange() ) {
+				invalidateTokens = true;
+			} else {
+				final List<PersistentGrantedAuthority> existingAuthorities =
+						new ArrayList<PersistentGrantedAuthority>
+								(existingModel.getAuthorities());
+				newModel = factory.from(obj);
+				invalidateTokens = !(areMatching(existingAuthorities, newModel.getAuthorities()));
+			}
+		}
+		if ( newModel == null ) {
+			newModel = factory.from(obj);
+		}
+		beforeWrite(newModel, obj);
+		newModel = getDao().save(newModel);
+		if ( invalidateTokens ) {
+			final Collection<OAuth2AccessToken> tokensByClientId =
+					consumerTokenService.findTokensByClientId(newModel.getClientId());
+			for ( OAuth2AccessToken token : tokensByClientId ) {
+				final OAuth2Authentication authN =
+						resourceServerTokenService.loadAuthentication(token.getValue());
+				if ( authN.isClientOnly() ) {
+					consumerTokenService.revokeToken(token.getValue());
+				}
+			}
+
+
+		}
+		return newModel;
+	}
+
+
+	private void beforeWrite(OAuth2Client client, OAuth2ClientTO from) {
+		if ( from.isSecretChange() ) {
+			encodeSecret(client);
+		}
+		ensureSchoolId(client);
+	}
+
+	private void encodeSecret(OAuth2Client client) {
+		client.setSecret(StringUtils.trimToNull(client.getSecret()));
+		if ( client.getSecret() != null ) {
+			client.setSecret(passwordEncoder.encode(client.getSecret()));
+		}
+	}
+
+	private void ensureSchoolId(OAuth2Client client) {
+		if (StringUtils.isBlank(client.getSchoolId()) ) {
+			client.setSchoolId(client.getClientId());
+		}
+	}
+
+	private boolean areMatching(List<PersistentGrantedAuthority> auth1,
+							 List<PersistentGrantedAuthority> auth2) {
+		if ( auth1 == auth2 ) {
+			return true;
+		}
+		if ( auth1 == null || auth2 == null ) {
+			return false;
+		}
+		return auth1.equals(auth2);
 	}
 }
