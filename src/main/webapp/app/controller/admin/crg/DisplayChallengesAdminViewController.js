56866f865befba3385e90f6f43badfc32015ff18
Dan McCallum <dan.mccallum@gmail.com>
2013-10-29 14:48:45 -0700

    SSP-1870 Change post-edit/create highlight color to green
    Previously was red, which suggested an error condition.
    
    This commit also centralizes that styling config into Constants.js.
    It's not really a "constant", per se, but we don't have anywhere else
    to put it.
    
    This commit also drops the duration of the highlight to 5s from 60s.

--- src/main/webapp/app/controller/admin/crg/DisplayChallengesAdminViewController.js
+++ src/main/webapp/app/controller/admin/crg/DisplayChallengesAdminViewController.js
@@ -60,10 +60,8 @@ Ext.define('Ssp.controller.admin.crg.DisplayChallengesAdminViewController', {
 					
                     var sn = me.getView().getView().getSelectedNodes()[0];
                     
-                    Ext.get(sn).highlight("ff0000", {
-                        attr: 'color',
-                        duration: 60000
-                    });
+                    Ext.get(sn).highlight(Ssp.util.Constants.SSP_EDITED_ROW_HIGHLIGHT_COLOR,
+                        Ssp.util.Constants.SSP_EDITED_ROW_HIGHLIGHT_OPTIONS);
                     me.adminSelectedIndex.set('value', -1);
                   
                     if (ptb) {
