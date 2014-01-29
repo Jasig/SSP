e48bde641ff83450aa7d9c5b8dbd82436ed05dae
archna.jindal <archna.jindal@sinclair.edu>
2014-01-29 10:24:24 -0500

    SSP-1041 'Anticipated Start Term' drop-down menu accepting text

--- src/main/webapp/app/view/person/AnticipatedStartDate.js
+++ src/main/webapp/app/view/person/AnticipatedStartDate.js
@@ -52,7 +52,15 @@ Ext.define('Ssp.view.person.AnticipatedStartDate', {
 		        mode: 'local',
 		        typeAhead: true,
 		        queryMode: 'local',
-		        allowBlank: true
+		        allowBlank: true,
+				forceSelection: true,
+				listeners:{
+					'change': function() {
+						if (this.getValue() === null) {
+							this.reset();
+							}
+					}
+				}
 			},{
 		        xtype: 'combobox',
 		        name: 'anticipatedStartYear',
