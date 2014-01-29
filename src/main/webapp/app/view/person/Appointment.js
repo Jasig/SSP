2bd452f06e73fc0bf3d41de5fe379df8d1c4c0ac
archna.jindal <archna.jindal@sinclair.edu>
2014-01-29 10:23:13 -0500

    SSP-1017 500 error occurs when email address too long

--- src/main/webapp/app/view/person/Appointment.js
+++ src/main/webapp/app/view/person/Appointment.js
@@ -17,73 +17,71 @@
  * under the License.
  */
 Ext.define('Ssp.view.person.Appointment', {
-	extend: 'Ext.form.Panel',
-	alias: 'widget.personappointment',
-    mixins: [ 'Deft.mixin.Injectable',
-              'Deft.mixin.Controllable'],
+    extend: 'Ext.form.Panel',
+    alias: 'widget.personappointment',
+    mixins: ['Deft.mixin.Injectable', 'Deft.mixin.Controllable'],
     controller: 'Ssp.controller.person.AppointmentViewController',
-	initComponent: function() {	
-		var me=this;
-		Ext.apply(me, 
-				{
-			    fieldDefaults: {
-			        msgTarget: 'side',
-			        labelAlign: 'right',
-			        labelWidth: 200
-			    },	
-			    border: 0,
-			    padding: 0,
-				items: [{
-			            xtype: 'fieldset',
-			            border: 0,
-			            padding: 0,
-			            title: '',
-			            defaultType: 'textfield',
-			            defaults: {
-			            	anchor: '50%'
-			            },
-			       items: [{
-				    	xtype: 'datefield',
-				    	fieldLabel: 'Appointment Date',
-				    	itemId: 'appointmentDateField',
-				    	altFormats: 'm/d/Y|m-d-Y',
-				    	invalidText: '{0} is not a valid date - it must be in the format: 06/21/2012 or 06-21-2012',
-				    	name: 'appointmentDate',
-				        allowBlank: false
-				    },{
-				        xtype: 'timefield',
-				        name: 'startTime',
-				        itemId: 'startTimeField',
-				        fieldLabel: 'Start Time',
-				        increment: 30,
-				        typeAhead: false,
-				        allowBlank: false
-				    },{
-				        xtype: 'timefield',
-				        name: 'endTime',
-				        itemId: 'endTimeField',
-				        fieldLabel: 'End Time',
-				        typeAhead: false,
-				        allowBlank: false,
-				        increment: 30
-				    },{
-			            xtype: 'checkboxfield',
-			            fieldLabel: 'Send Student Intake Request',
-				        name: 'studentIntakeRequested',
-			            itemId: 'studentIntakeRequestedField',
-			            inputValue: true
-				    },{
-			            xtype: 'textfield',
-			            fieldLabel: 'Also Send Student Intake Request To Email',
-				        name: 'intakeEmail',
-			            itemId: 'intakeEmailField',
-			            hidden: true
-				    }
-				    
-				    ]
-			    }]
-			});
-		
-		return me.callParent(arguments);
-	}
+    initComponent: function(){
+        var me = this;
+        Ext.apply(me, {
+            fieldDefaults: {
+                msgTarget: 'side',
+                labelAlign: 'right',
+                labelWidth: 200
+            },
+            border: 0,
+            padding: 0,
+            items: [{
+                xtype: 'fieldset',
+                border: 0,
+                padding: 0,
+                title: '',
+                defaultType: 'textfield',
+                defaults: {
+                    anchor: '50%'
+                },
+                items: [{
+                    xtype: 'datefield',
+                    fieldLabel: 'Appointment Date',
+                    itemId: 'appointmentDateField',
+                    altFormats: 'm/d/Y|m-d-Y',
+                    invalidText: '{0} is not a valid date - it must be in the format: 06/21/2012 or 06-21-2012',
+                    name: 'appointmentDate',
+                    allowBlank: false
+                }, {
+                    xtype: 'timefield',
+                    name: 'startTime',
+                    itemId: 'startTimeField',
+                    fieldLabel: 'Start Time',
+                    increment: 30,
+                    typeAhead: false,
+                    allowBlank: false
+                }, {
+                    xtype: 'timefield',
+                    name: 'endTime',
+                    itemId: 'endTimeField',
+                    fieldLabel: 'End Time',
+                    typeAhead: false,
+                    allowBlank: false,
+                    increment: 30
+                }, {
+                    xtype: 'checkboxfield',
+                    fieldLabel: 'Send Student Intake Request',
+                    name: 'studentIntakeRequested',
+                    itemId: 'studentIntakeRequestedField',
+                    inputValue: true
+                }, {
+                    //xtype: 'textfield',
+                    fieldLabel: 'Also Send Student Intake Request To Email',
+                    name: 'intakeEmail',
+                    itemId: 'intakeEmailField',
+                    hidden: true,
+                    maxLength: 100,
+                    vtype: 'email'
+                }]
+            }]
+        });
+        
+        return me.callParent(arguments);
+    }
 });
