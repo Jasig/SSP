e151df459cfed0853178087c846e2d1ecd8ff18b
Jim Stanley <jstanley@unicon.net>
2014-01-29 11:30:52 -0700

    NOJIRA removed excess import

--- src/main/java/org/jasig/ssp/model/Task.java
+++ src/main/java/org/jasig/ssp/model/Task.java
@@ -29,7 +29,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
