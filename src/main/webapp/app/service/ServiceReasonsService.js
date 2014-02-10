fa53f585ca3c0022aff9982d1600e3e11715f9eb
archna.jindal <archna.jindal@sinclair.edu>
2014-02-10 07:35:19 -0500

    SSP-2078 Can't add student against fresh database

---
+++ src/main/webapp/app/service/ServiceReasonsService.js
@@ -0,0 +1,55 @@
+/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+Ext.define('Ssp.service.ServiceReasonsService', {  
+    extend: 'Ssp.service.AbstractService',   		
+    mixins: [ 'Deft.mixin.Injectable'],
+    inject: {
+    	apiProperties: 'apiProperties'
+    },
+    initComponent: function() {
+		return this.callParent( arguments );
+    },
+    
+    getBaseUrl: function(){
+		var me=this;
+		var baseUrl = me.apiProperties.createUrl( me.apiProperties.getItemUrl('serviceReason') );
+		return baseUrl;
+    },
+
+    getAll: function( callbacks ){
+    	var me=this;
+	    var success = function( response, view ){
+	    	var r = Ext.decode(response.responseText);
+			callbacks.success( r, callbacks.scope );
+	    };
+
+	    var failure = function( response ){
+	    	me.apiProperties.handleError( response );	    	
+	    	callbacks.failure( response, callbacks.scope );
+	    };
+	    
+		me.apiProperties.makeRequest({
+			url: me.getBaseUrl() + "/?limit=-1&status=ALL",
+			method: 'GET',
+			successFunc: success,
+			failureFunc: failure,
+			scope: me
+		});
+    }
+});
