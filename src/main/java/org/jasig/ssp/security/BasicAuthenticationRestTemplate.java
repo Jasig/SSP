2ee91bed664f1299b3017498b707f5cecdeea5db
Jim Stanley <jstanley@unicon.net>
2014-03-06 13:17:27 -0700

    SSP-1915 Update To BasicAuthenticationRestTemplate to set client host,port and accept gzip

--- src/main/java/org/jasig/ssp/security/BasicAuthenticationRestTemplate.java
+++ src/main/java/org/jasig/ssp/security/BasicAuthenticationRestTemplate.java
@@ -2,18 +2,42 @@ package org.jasig.ssp.security;
 
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DecompressingHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 public class BasicAuthenticationRestTemplate extends RestTemplate {
-    public BasicAuthenticationRestTemplate(String username, String password) {
+	
+    public BasicAuthenticationRestTemplate(String username, String password, String clientIpAddress, Integer clientPort, Boolean acceptEncodingGzip) {
+    	    	
         DefaultHttpClient clientd = new DefaultHttpClient();
-        clientd.getCredentialsProvider().setCredentials(new AuthScope(null, -1), 
-        		new UsernamePasswordCredentials(username, password));
+        addClientAddress( clientd,  clientIpAddress,  clientPort);
+        addAuthentication( clientd,  username,  password);
+       
+        setRequestFactory(new HttpComponentsClientHttpRequestFactory(addGzip(clientd, acceptEncodingGzip)));
+    }
+    
+   
+    
+    public BasicAuthenticationRestTemplate(String username, String password) {
+    	DefaultHttpClient clientd = new DefaultHttpClient();
+        addAuthentication( clientd,  username,  password);
+       
         setRequestFactory(new HttpComponentsClientHttpRequestFactory(clientd));
+    }
+    
+    public BasicAuthenticationRestTemplate(String username, String password, Boolean acceptEncodingGzip) {
+    	DefaultHttpClient clientd = new DefaultHttpClient();
+        addAuthentication( clientd,  username,  password);
+       
+        setRequestFactory(new HttpComponentsClientHttpRequestFactory(addGzip(clientd, acceptEncodingGzip)));
     }
     
 	@SuppressWarnings("unchecked")
@@ -23,4 +47,25 @@ public class BasicAuthenticationRestTemplate extends RestTemplate {
 		return getForObject(url, objectClass, params);
 	}
 	
+	 private HttpClient addClientAddress(HttpClient clientd, String clientIpAddress, Integer clientPort){
+	    	if(StringUtils.isNotBlank(clientIpAddress) && clientPort != null){
+	    		HttpHost proxy = new HttpHost(clientIpAddress, clientPort);
+	    		clientd.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+	    	}
+	         return clientd;
+	    }
+	    
+	    private HttpClient addAuthentication(DefaultHttpClient clientd, String username, String password){
+	    	clientd.getCredentialsProvider().setCredentials(new AuthScope(null, -1), 
+	        		new UsernamePasswordCredentials(username, password));
+	    	return clientd;
+	   }
+	    
+	    private HttpClient addGzip(HttpClient clientd, Boolean acceptEncodingGzip){
+	    	 
+	        if(acceptEncodingGzip){
+	        	return new DecompressingHttpClient(clientd);
+	        }
+	        return clientd;
+	    }
 }
