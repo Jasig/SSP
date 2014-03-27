Jasig/SSP

commit a1be1601cd63381ded6b7b68232310851e4b27f8

SSP-2084  Initial commit for notification of out of compliance early alerts

src/main/java/org/jasig/ssp/web/api/PersonController.java
@@ -33,6 +33,7 @@ import org.jasig.ssp.security.permissions.Permission;
 import org.jasig.ssp.service.ObjectNotFoundException;
 import org.jasig.ssp.service.PersonService;
 import org.jasig.ssp.service.SecurityService;
+import org.jasig.ssp.service.external.ExternalPersonService;
 import org.jasig.ssp.transferobject.CoachPersonLiteTO;
 import org.jasig.ssp.transferobject.PagedResponse;
 import org.jasig.ssp.transferobject.PersonLiteTO;
@@ -70,6 +71,9 @@ public class PersonController extends AbstractBaseController {
 
 	@Autowired
 	private transient PersonService service;
+	
+	@Autowired
+	private transient ExternalPersonService externalPersonService;
 
 	@Autowired
 	private transient PersonTOFactory factory;
@@ -266,6 +270,7 @@ public class PersonController extends AbstractBaseController {
 					conflict = null;
 					createdModel = service.create(model);
 					if (null != createdModel) {
+						externalPersonService.updatePersonFromExternalPerson(createdModel);
 						return new PersonTO(createdModel);
 					}
 				} catch ( PersonExistsException e ) {

src/main/java/org/jasig/ssp/dao/EarlyAlertDao.java
@@ -30,6 +30,7 @@ import java.util.UUID;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.criterion.ProjectionList;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
@@ -533,6 +534,58 @@ public class EarlyAlertDao extends
 		
 		return total;
 
+	}
+	
+	public List<EarlyAlert> getResponseDueEarlyAlerts(Date lastResponseDate){
+		String sql = "select distinct ea " + responseQuery();
+		final Query query = createHqlQuery(sql);
+		query.setParameter("lastResponseDate", lastResponseDate);
+		query.setParameter("objectStatus", ObjectStatus.ACTIVE);
+		return (List<EarlyAlert>)query.list();
+	}
+	
+	public Map<UUID, Number> getResponsesDueCountEarlyAlerts(
+			@NotNull final Collection<UUID> personIds, Date lastResponseDate) {
+		List<List<UUID>> batches = prepareBatches(personIds);
+		List<Object[]> results = new ArrayList<Object[]>();
+		Map<UUID,Number> responsesDuePerPerson = new HashMap<UUID,Number>();
+		for(List<UUID> batch:batches){
+			results.addAll(getResponsesBatchDueCountEarlyAlerts(batch, lastResponseDate));
+		}
+		
+
+		// put query results into return value
+		for (final Object[] result : results) {
+			responsesDuePerPerson.put((UUID) result[0], (Number) result[1]);
+		}
+
+		// ensure all people IDs that were request exist in return Map
+		for (final UUID id : personIds) {
+			if (!responsesDuePerPerson.containsKey(id)) {
+				responsesDuePerPerson.put(id, 0);
+			}
+		}
+		return responsesDuePerPerson;
+	}
+	
+	public List<Object[]> getResponsesBatchDueCountEarlyAlerts(List<UUID> students, Date lastResponseDate){
+		String sql = "select distinct ea.person.id, count(ea) " +  responseQuery() 
+				+ " and ea.person.id in :personIds group by ea.person.id";
+		final Query query = createHqlQuery(sql);
+		query.setParameter("objectStatus", ObjectStatus.ACTIVE);
+		query.setParameter("lastResponseDate", lastResponseDate);
+		query.setParameterList("personIds", students);
+		return (List<Object[]>)query.list();
+	}
+	
+	private  String  responseQuery(){
+		return "from EarlyAlert as ea where ((ea.closedDate is null and ea.objectStatus = :objectStatus "
+				+ "and ea.lastResponseDate is null and ea.createdDate < :lastResponseDate) or "
+				+ "(ea.closedDate is null and ea.objectStatus = :objectStatus and ea.lastResponseDate < :lastResponseDate)) ";
+				/*+ "and  (((select max(ear.modifiedDate) from EarlyAlertResponse as ear "
+			+ "where ear.earlyAlertId = ea.id) is empty)"
+				+ "or (select max(ear.modifiedDate) from EarlyAlertResponse as ear2 "
+			+ "where ear2.earlyAlertId = ea.id) <= :lastResponseDate)";*/
 	}
 	
 	private Criteria setPersonCriteria(Criteria criteria, PersonSearchFormTO personSearchForm){

src/main/java/org/jasig/ssp/model/PersonSearchResult2.java
@@ -56,6 +56,8 @@ public class PersonSearchResult2 {
 
 	private int numberOfEarlyAlerts;
 	
+	private int numberEarlyAlertResponsesRequired;
+	
 	private UUID id;
 	
 	private UUID coachId;
@@ -214,5 +216,18 @@ public class PersonSearchResult2 {
 
 	public void setNumberOfEarlyAlerts(int numberOfEarlyAlerts) {
 		this.numberOfEarlyAlerts = numberOfEarlyAlerts;
+	}
+	
+	public void setNumberEarlyAlertResponsesRequired(final Number numberEarlyAlertResponsesRequired) {
+		this.numberEarlyAlertResponsesRequired = numberEarlyAlertResponsesRequired.intValue();
+	}
+
+
+	public int getNumberEarlyAlertResponsesRequired() {
+		return numberEarlyAlertResponsesRequired;
+	}
+
+	public void setNumberEarlyAlertResponsesRequired(int numberEarlyAlertResponsesRequired) {
+		this.numberEarlyAlertResponsesRequired = numberEarlyAlertResponsesRequired;
 	}
 }

src/main/java/org/jasig/ssp/model/reference/MessageTemplate.java
@@ -80,8 +80,11 @@ public class MessageTemplate
 	public static final UUID MAP_STATUS_REPORT_ID = UUID
 			.fromString("3da780e7-b7df-43b2-8988-fc51ef03f7c0");
 	
-	public static final UUID EARLYALERT_RESPONSE_TO_REFERRAL_SOURCE = UUID.
+	public static final UUID EARLYALERT_RESPONSE_TO_REFERRAL_SOURCE_ID = UUID.
 			fromString("d6d1f68a-0533-426f-bd0b-d129a92edf81");
+	
+	public static final UUID EARLYALERT_RESPONSE_REQUIRED_ID = UUID.
+			fromString("bfae36f8-53d0-486f-8bc5-a9d98beb544e");
 
 	@NotNull
 	@Column(name = "subject", nullable = false, length = 250)

src/main/java/org/jasig/ssp/service/EarlyAlertService.java
@@ -23,6 +23,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
 import javax.mail.SendFailedException;
 import javax.validation.constraints.NotNull;
 
@@ -165,6 +166,8 @@ public interface EarlyAlertService
 
 	PagingWrapper<EarlyAlertStudentReportTO> getStudentsEarlyAlertCountSetForCritera(EarlyAlertStudentSearchTO searchForm, SortingAndPaging createForSingleSort);
 	
-	public PagingWrapper<EntityStudentCountByCoachTO> getStudentEarlyAlertCountByCoaches(EntityCountByCoachSearchForm form);
+	PagingWrapper<EntityStudentCountByCoachTO> getStudentEarlyAlertCountByCoaches(EntityCountByCoachSearchForm form);
 
+	Map<UUID,Number> getResponsesDueCountEarlyAlerts(List<UUID> personIds);
+	void sendAllEarlyAlertReminderNotifications();
 }

src/main/java/org/jasig/ssp/service/ScheduledTaskWrapperService.java
@@ -33,4 +33,6 @@ public interface ScheduledTaskWrapperService {
 	public void sendTaskReminders();
 
 	public void calcMapStatusReports();
+	
+	public void sendEarlyAlertReminders();
 }

src/main/java/org/jasig/ssp/service/impl/EarlyAlertServiceImpl.java
@@ -19,8 +19,13 @@
 package org.jasig.ssp.service.impl; // NOPMD by jon.adams
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -63,10 +68,13 @@ import org.jasig.ssp.service.reference.EarlyAlertSuggestionService;
 import org.jasig.ssp.service.reference.MessageTemplateService;
 import org.jasig.ssp.service.reference.ProgramStatusService;
 import org.jasig.ssp.service.reference.StudentTypeService;
+import org.jasig.ssp.transferobject.EarlyAlertTO;
+import org.jasig.ssp.transferobject.messagetemplate.EarlyAlertMessageTemplateTO;
 import org.jasig.ssp.transferobject.reports.EarlyAlertStudentReportTO;
 import org.jasig.ssp.transferobject.reports.EarlyAlertStudentSearchTO;
 import org.jasig.ssp.transferobject.reports.EntityCountByCoachSearchForm;
 import org.jasig.ssp.transferobject.reports.EntityStudentCountByCoachTO;
+import org.jasig.ssp.util.collections.Pair;
 import org.jasig.ssp.util.sort.PagingWrapper;
 import org.jasig.ssp.util.sort.SortingAndPaging;
 import org.jasig.ssp.web.api.validation.ValidationException;
@@ -76,6 +84,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
@@ -739,6 +748,112 @@ public class EarlyAlertServiceImpl extends // NOPMD
 	@Override
 	public Long getEarlyAlertCountSetForCritera(EarlyAlertStudentSearchTO searchForm){
 		return dao.getEarlyAlertCountSetForCritera(searchForm);
+	}
+	
+	@Override
+	public void sendAllEarlyAlertReminderNotifications() {
+		Date lastResponseDate = getMinimumResponseComplianceDate();
+		// if no responseDate is given no emails are sent
+		if(lastResponseDate == null)
+			return;
+		List<EarlyAlert> eaOutOfCompliance = dao.getResponseDueEarlyAlerts(lastResponseDate);
+		Map<Person, List<EarlyAlertMessageTemplateTO>> easByCoach = new HashMap<Person, List<EarlyAlertMessageTemplateTO>>();
+		for(EarlyAlert earlyAlert: eaOutOfCompliance){
+			Person coach = earlyAlert.getPerson().getCoach();
+			if(easByCoach.containsKey(coach)){
+				List<EarlyAlertMessageTemplateTO> coachEarlyAlerts = easByCoach.get(coach);
+				coachEarlyAlerts.add(new EarlyAlertMessageTemplateTO(earlyAlert));
+			}else{
+				easByCoach.put(coach, Lists.newArrayList(new EarlyAlertMessageTemplateTO(earlyAlert)));
+			}
+		}
+		for(Person coach: easByCoach.keySet()){
+			Map<String,Object> messageParams = new HashMap<String,Object>();
+			
+			Collections.sort(easByCoach.get(coach), new Comparator<EarlyAlertTO>() {
+		        @Override public int compare(EarlyAlertTO p1, EarlyAlertTO p2) {
+		        	Date p1Date = p1.getLastResponseDate();
+		        	if(p1Date == null)
+		        		p1Date = p1.getCreatedDate();
+		        	Date p2Date = p2.getLastResponseDate();
+		        	if(p2Date == null)
+		        		p2Date = p2.getCreatedDate();
+		            return p1Date.compareTo(p2Date);
+		        }
+
+		    });
+			
+			Integer daysSince1900ResponseExpected =  daysSince1900(lastResponseDate);
+			List<Pair<EarlyAlertMessageTemplateTO,Integer>> earlyAlertTOPairs = new ArrayList<Pair<EarlyAlertMessageTemplateTO,Integer>>();
+			for(EarlyAlertMessageTemplateTO ea:easByCoach.get(coach)){
+				Integer daysOutOfCompliance;
+				if(ea.getLastResponseDate() != null){
+					daysOutOfCompliance = daysSince1900ResponseExpected - daysSince1900(ea.getLastResponseDate());
+				}else{
+				    daysOutOfCompliance = daysSince1900ResponseExpected - daysSince1900(ea.getCreatedDate());
+				}
+				
+				// Just in case attempt to only send emails for EA full day out of compliance
+				if(daysOutOfCompliance >= 0)
+					earlyAlertTOPairs.add(new Pair<EarlyAlertMessageTemplateTO,Integer>(ea, daysOutOfCompliance));
+			}
+			messageParams.put("earlyAlertTOPairs", earlyAlertTOPairs);
+			messageParams.put("coach", coach);
+			messageParams.put("termToRepresentEarlyAlert",
+					configService.getByNameEmpty("term_to_represent_early_alert"));
+			SubjectAndBody subjAndBody = messageTemplateService.createEarlyAlertResponseRequiredToCoachMessage(messageParams);
+			try{
+				messageService.createMessage(coach, null, subjAndBody);
+			}catch(Exception exp){
+				LOGGER.error("Unable to send reminder emails to coach: " + coach.getFullName() + "\n", exp);
+			}
+		}
+		
+	}
+	
+	public Map<UUID,Number> getResponsesDueCountEarlyAlerts(List<UUID> personIds){
+		Date lastResponseDate = getMinimumResponseComplianceDate();
+		if(lastResponseDate == null)
+			return new HashMap<UUID,Number>();
+		return dao.getResponsesDueCountEarlyAlerts(personIds, lastResponseDate);
+	}
+	
+	private Date getMinimumResponseComplianceDate(){
+		final String numVal = configService
+				.getByNameNull("maximum_days_before_early_alert_response");
+		if(StringUtils.isBlank(numVal))
+			return null;
+		Integer allowedDaysPastResponse = Integer.parseInt(numVal);
+		
+		Date lastResponseDate = new Date();
+		
+		Calendar cal = Calendar.getInstance();
+	    cal.setTime(lastResponseDate);
+	    cal.add(Calendar.DAY_OF_MONTH, -allowedDaysPastResponse);
+	    return cal.getTime();
+				
+	}
+	
+	private  int daysSince1900(Date date) {
+	    Calendar c = new GregorianCalendar();
+	    c.setTime(date);
+
+	    int year = c.get(Calendar.YEAR);
+	    if (year < 1900 || year > 2099) {
+	        throw new IllegalArgumentException("daysSince1900 - Date must be between 1900 and 2099");
+	    }
+	    year -= 1900;
+	    int month = c.get(Calendar.MONTH) + 1;
+	    int days = c.get(Calendar.DAY_OF_MONTH);
+
+	    if (month < 3) {
+	        month += 12;
+	        year--;
+	    }
+	    int yearDays = (int) (year * 365.25);
+	    int monthDays = (int) ((month + 1) * 30.61);
+
+	    return (yearDays + monthDays + days - 63);
 	}
 	
 }

src/main/java/org/jasig/ssp/service/impl/PersonSearchServiceImpl.java
@@ -111,6 +111,7 @@ public class PersonSearchServiceImpl implements PersonSearchService {
 		return new PagingWrapper<PersonSearchResult>(people.getResults(),
 				personSearchResults);
 	}
+	
 
 	@Override
 	public PagingWrapper<PersonSearchResult2> search2(PersonSearchRequest from) {
@@ -127,6 +128,9 @@ public class PersonSearchServiceImpl implements PersonSearchService {
 
 		final Map<UUID, Number> earlyAlertCounts = earlyAlertService
 				.getCountOfActiveAlertsForPeopleIds(peopleIds);
+		
+		final Map<UUID, Number> numberEarlyAlertResponsesRequired = earlyAlertService.
+				getResponsesDueCountEarlyAlerts(peopleIds);
 
 		for (final PersonSearchResult2 record : results) {
 			if (appts.containsKey(record.getId())) {
@@ -136,6 +140,11 @@ public class PersonSearchServiceImpl implements PersonSearchService {
 
 			if (earlyAlertCounts.containsKey(record.getId())) {
 				record.setNumberOfEarlyAlerts(earlyAlertCounts.get(record
+						.getId()));
+			}
+			
+			if (numberEarlyAlertResponsesRequired.containsKey(record.getId())) {
+				record.setNumberEarlyAlertResponsesRequired(numberEarlyAlertResponsesRequired.get(record
 						.getId()));
 			}
 		}

commit 78f90df12e9cb173a285cdbe8fa66c5da5e6e12e

SSP-2244 inadvertently committed with a1be1601cd63381ded6b7b68232310851e4b27f8, regressed

src/main/java/org/jasig/ssp/web/api/PersonController.java
@@ -270,7 +270,6 @@ public class PersonController extends AbstractBaseController {
 					conflict = null;
 					createdModel = service.create(model);
 					if (null != createdModel) {
-						externalPersonService.updatePersonFromExternalPerson(createdModel);
 						return new PersonTO(createdModel);
 					}
 				} catch ( PersonExistsException e ) {