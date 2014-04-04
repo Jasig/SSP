4c51c4e4dcd80c7a1c92a807c15a6ec8b83b8742
Dan McCallum <dan.mccallum@gmail.com>
2014-04-04 13:01:19 -0700

    SSP-2304 Reflect task name in thread name and Logback MDC

--- src/main/java/org/jasig/ssp/service/impl/ScheduledTaskWrapperServiceImpl.java
+++ src/main/java/org/jasig/ssp/service/impl/ScheduledTaskWrapperServiceImpl.java
@@ -38,6 +38,7 @@ import org.jasig.ssp.util.collections.Pair;
 import org.jasig.ssp.util.sort.PagingWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -75,6 +76,13 @@ public class ScheduledTaskWrapperServiceImpl
 
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(ScheduledTaskWrapperServiceImpl.class);
+
+	public static final String TASK_NAME_MDC_KEY = "ssp.taskName";
+	public static final String SEND_MESSAGES_TASK_NAME = "send-messages";
+	public static final String SYNC_COACHES_TASK_NAME = "sync-coaches";
+	public static final String SYNC_EXTERNAL_PERSONS_TASK_NAME = "sync-external-persons";
+	public static final String CALC_MAP_STATUS_REPORTS_TASK_NAME = "calc-map-status-reports";
+	public static final String SEND_TASK_REMINDERS_TASK_NAME = "send-task-reminders";
 
 	private static final String EVERY_DAY_1_AM = "0 0 1 * * *";
 	private static final String EVERY_DAY_3_AM = "0 0 3 * * *";
@@ -630,6 +638,30 @@ public class ScheduledTaskWrapperServiceImpl
 		};
 	}
 
+	protected Runnable withTaskName(final String taskName, final Runnable work) {
+		return new Runnable() {
+			@Override
+			public void run() {
+				final String currentThreadName = Thread.currentThread().getName();
+				final String currentMdcEntry = MDC.get(TASK_NAME_MDC_KEY);
+				try {
+					final String newThreadName = currentThreadName == null ? taskName : currentThreadName + ":" + taskName;
+					Thread.currentThread().setName(newThreadName);
+					final String newMdcEntry = currentMdcEntry == null ? taskName : currentMdcEntry + ":" + taskName;
+					MDC.put(TASK_NAME_MDC_KEY, newMdcEntry);
+					work.run();
+				} finally {
+					if ( currentMdcEntry == null ) {
+						MDC.remove(TASK_NAME_MDC_KEY);
+					} else {
+						MDC.put(TASK_NAME_MDC_KEY, currentMdcEntry);
+					}
+					Thread.currentThread().setName(currentThreadName);
+				}
+			}
+		};
+	}
+
 	/**
 	 * Wraps the given {@code Runnable} in the "standard" decorators you'd
 	 * typically need for execution of a background task and returns the
@@ -637,8 +669,8 @@ public class ScheduledTaskWrapperServiceImpl
 	 *
 	 * @param work
 	 */
-	protected Runnable withTaskContext(Runnable work) {
-		return withHibernateSession(withTaskCleanup(withMaybeSudo(work)));
+	protected Runnable withTaskContext(String taskName, Runnable work) {
+		return withTaskName(taskName, withHibernateSession(withTaskCleanup(withMaybeSudo(work))));
 	}
 
 	/**
@@ -650,15 +682,15 @@ public class ScheduledTaskWrapperServiceImpl
 	 *
 	 * @param work
 	 */
-	protected void execWithTaskContext(Runnable work) {
-		withTaskContext(work).run();
+	protected void execWithTaskContext(String taskName, Runnable work) {
+		withTaskContext(taskName, work).run();
 	}
 
 	@Override
 	@Scheduled(fixedDelay = 150000)
 	// run 2.5 minutes after the end of the last invocation
 	public void sendMessages() {
-		execWithTaskContext(new Runnable() {
+		execWithTaskContext(SEND_MESSAGES_TASK_NAME, new Runnable() {
 			@Override
 			public void run() {
 				messageService.sendQueuedMessages();
@@ -670,7 +702,7 @@ public class ScheduledTaskWrapperServiceImpl
 	@Scheduled(fixedDelay = 300000)
 	// run every 5 minutes
 	public void syncCoaches() {
-		execWithTaskContext(new Runnable() {
+		execWithTaskContext(SYNC_COACHES_TASK_NAME, new Runnable() {
 			@Override
 			public void run() {
 				if (!(scheduledCoachSyncEnabled)) {
@@ -691,7 +723,7 @@ public class ScheduledTaskWrapperServiceImpl
 	 */
 	@Override
 	public void syncExternalPersons() {
-		execWithTaskContext(new Runnable() {
+		execWithTaskContext(SYNC_EXTERNAL_PERSONS_TASK_NAME, new Runnable() {
 			@Override
 			public void run() {
 				externalPersonSyncTask.exec();
@@ -701,7 +733,7 @@ public class ScheduledTaskWrapperServiceImpl
 
 	@Override
 	public void calcMapStatusReports() {
-		execWithTaskContext(new Runnable() {
+		execWithTaskContext(CALC_MAP_STATUS_REPORTS_TASK_NAME,new Runnable() {
 			@Override
 			public void run() {
 				mapStatusReportCalcTask.exec();
@@ -713,7 +745,7 @@ public class ScheduledTaskWrapperServiceImpl
 	@Scheduled(cron = "0 0 1 * * *")
 	// run at 1 am every day
 	public void sendTaskReminders() {
-		execWithTaskContext(new Runnable() {
+		execWithTaskContext(SEND_TASK_REMINDERS_TASK_NAME, new Runnable() {
 			@Override
 			public void run() {
 				taskService.sendAllTaskReminderNotifications();
