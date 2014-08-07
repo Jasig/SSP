1079e48451e12e68a000fee32617f7af457414cd
Jim Stanley <jstanley@unicon.net>
2014-08-07 13:32:09 -0700

    Revert "NOJIRA"
    This reverts commit c4c7941e9ae7919bd4dc511e4a6942597f1796c2.

--- src/main/java/org/jasig/ssp/dao/DirectoryPersonSearchDao.java
+++ src/main/java/org/jasig/ssp/dao/DirectoryPersonSearchDao.java
@@ -89,21 +89,15 @@ public class DirectoryPersonSearchDao  {
 		
 		if(isPostgresSession()){
 			try{
-				LOGGER.info("REFRESH_MV_DIRECTORY_PERSON started");
 				Query query = sessionFactory.getCurrentSession().createSQLQuery("select REFRESH_MV_DIRECTORY_PERSON();");
-				LOGGER.info("REFRESH_MV_DIRECTORY_PERSON ended successfully");
 				query.list();
 			}catch(Exception exp){
-				LOGGER.info("REFRESH_MV_DIRECTORY_PERSON started after catch.");
 				Query query = sessionFactory.getCurrentSession().createSQLQuery("exec REFRESH_MV_DIRECTORY_PERSON;");
 				query.list();
-				LOGGER.info("REFRESH_MV_DIRECTORY_PERSON ended succesfully after catch.");
 			}
 		}else{
-			LOGGER.info("REFRESH_MV_DIRECTORY_PERSON started");
 			Query query = sessionFactory.getCurrentSession().createSQLQuery("exec REFRESH_MV_DIRECTORY_PERSON;");
 			query.list();
-			LOGGER.info("REFRESH_MV_DIRECTORY_PERSON ended successfully");
 		}
 	}
 	
@@ -111,21 +105,15 @@ public class DirectoryPersonSearchDao  {
 		
 		if(isPostgresSession()){
 			try{
-				LOGGER.info("REFRESH_MV_DIRECTORY_PERSON_BLUE started");
 				Query query = sessionFactory.getCurrentSession().createSQLQuery("select REFRESH_MV_DIRECTORY_PERSON_BLUE();");
 				query.list();
-				LOGGER.info("REFRESH_MV_DIRECTORY_PERSON_BLUE ended");
 			}catch(Exception exp){
-				LOGGER.info("REFRESH_MV_DIRECTORY_PERSON_BLUE started after catch");
 				Query query = sessionFactory.getCurrentSession().createSQLQuery("exec REFRESH_MV_DIRECTORY_PERSON_BLUE;");
 				query.list();
-				LOGGER.info("REFRESH_MV_DIRECTORY_PERSON_BLUE ended successfully after catch.");
 			}
 		}else{
-			LOGGER.info("REFRESH_MV_DIRECTORY_PERSON_BLUE started");
 			Query query = sessionFactory.getCurrentSession().createSQLQuery("exec REFRESH_MV_DIRECTORY_PERSON_BLUE;");
 			query.list();
-			LOGGER.info("REFRESH_MV_DIRECTORY_PERSON_BLUE ended successfully");
 		}
 	}
 	
@@ -285,9 +273,6 @@ public class DirectoryPersonSearchDao  {
         buildActualStartTerm(personSearchRequest, filterTracker, stringBuilder);
 		
 		buildEarlyAlertCriteria(personSearchRequest,filterTracker, stringBuilder);
-		
-		//watchList
-		buildWatchList(personSearchRequest,filterTracker, stringBuilder);
 		
 		addProgramStatusRequired(personSearchRequest, filterTracker, stringBuilder);
 	}
@@ -456,16 +441,6 @@ public class DirectoryPersonSearchDao  {
 	private boolean hasSpecialServiceGroup(PersonSearchRequest personSearchRequest) {
 		return personSearchRequest.getSpecialServiceGroup() != null;
 	}
-	
-	private void buildWatchList(PersonSearchRequest personSearchRequest,
-			FilterTracker filterTracker, StringBuilder stringBuilder) {
-		if(hasWatchStudent(personSearchRequest))
-		{
-			appendAndOrWhere(stringBuilder,filterTracker);
-			stringBuilder.append(" ws.person = :watcher ");		
-			stringBuilder.append(" and ws.student.id = dp.personId ");			
-		}
-	}
 
 	
 	private Map<String, Object> getBindParams(PersonSearchRequest personSearchRequest, Term currentTerm)
@@ -523,16 +498,12 @@ public class DirectoryPersonSearchDao  {
 			}			
 		}
 		
-		if(hasCoach(personSearchRequest) || hasMyCaseload(personSearchRequest) )
+		if(hasCoach(personSearchRequest) || hasMyCaseload(personSearchRequest))
 		{
 			Person coach = personSearchRequest.getMyCaseload() != null && personSearchRequest.getMyCaseload() ? securityService.currentlyAuthenticatedUser().getPerson() : personSearchRequest.getCoach();
 			params.put("coachId", coach.getId());
 		}
-		if(hasWatchStudent(personSearchRequest))
-		{
-			Person watcher = personSearchRequest.getMyWatchList() != null && personSearchRequest.getMyWatchList() ? securityService.currentlyAuthenticatedUser().getPerson() : personSearchRequest.getWatcher();
-			params.put("watcher", watcher);
-		}		
+		
 		if(hasDeclaredMajor(personSearchRequest))
 		{
 			params.put("programCode", personSearchRequest.getDeclaredMajor());
@@ -815,16 +786,7 @@ public class DirectoryPersonSearchDao  {
 		{
 			stringBuilder.append(", MapStatusReport msr ");
 		}
-		if(hasWatchStudent(personSearchRequest))
-		{
-			stringBuilder.append(", WatchStudent ws ");
-		}		
-		
 		return true;
-	}
-
-	private boolean hasWatchStudent(PersonSearchRequest personSearchRequest) {
-		return personSearchRequest.getMyWatchList() != null;
 	}
 
 	private boolean hasGpaCriteria(PersonSearchRequest personSearchRequest) 
