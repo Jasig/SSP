e1e28cc581102d8b2787776de7605dfa871cd420
aarland <aarland@unicon.net>
2014-08-07 11:09:56 -0600

    SSP-2694 - external person sync not completing

--- src/main/java/org/jasig/ssp/service/external/impl/ExternalPersonSyncTaskImpl.java
+++ src/main/java/org/jasig/ssp/service/external/impl/ExternalPersonSyncTaskImpl.java
@@ -197,7 +197,6 @@ public class ExternalPersonSyncTaskImpl implements ExternalPersonSyncTask {
 							" person records have already been processed"
 							+ " in this execution. Will resume at index"
 							+ " [{}] on next execution.", nextPersonIndex);
-					break;
 				}
 
 				// Mismatch between the PagedResponse and SortingAndPaging
