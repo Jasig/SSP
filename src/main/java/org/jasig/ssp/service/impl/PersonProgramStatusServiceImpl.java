851edf73224386fd6d18959d56363ec0fd979493
Dan McCallum <dan.mccallum@gmail.com>
2014-08-15 10:45:50 -0700

    SSP-2710 Expire persistent prog status before creating new
    Transitioning a person's program status requires a SQL update
    to expire the old and an insert to create the new. A trigger
    fires after each to update mv_directory_person. If the
    insert runs first, the trigger will error out b/c the person
    has two active program statuses. Hibernate has no idea that
    we have a business rule here, and the two program status
    records are completely unrelated to each other from Hib's
    perspective. So it makes no guarantees about the order of the
    insert and update when the next session flush occurs. In
    some or all cases we were seeing the insert run first.
    
    This patch forces the session to flush after the update.

--- src/main/java/org/jasig/ssp/service/impl/PersonProgramStatusServiceImpl.java
+++ src/main/java/org/jasig/ssp/service/impl/PersonProgramStatusServiceImpl.java
@@ -40,6 +40,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * PersonProgramStatus service implementation
@@ -137,7 +138,11 @@ public class PersonProgramStatusServiceImpl extends
 		PersonProgramStatus pps = getActiveExcluding(person, savingStatus);
 		if ( pps != null ) {
 			pps.setExpirationDate(new Date());
-			dao.save(pps);
+			// make sure this runs before a subsequent insert (if any), else
+			// the next Hib session flush might run the insert before the
+			// update and we'd get two active statuses at the same time,
+			// which will break db triggers
+			dao.saveAndFlush(pps);
 		}
 	}
 
