8ffddb67edc1914c67d72b2ff7d3b5a1ea10d4f7
Dan McCallum <dan.mccallum@gmail.com>
2014-09-09 14:10:00 -0700

    SSP-2779 Restore ILP flag rendering in Disability report

--- src/main/java/org/jasig/ssp/transferobject/reports/BaseStudentReportTO.java
+++ src/main/java/org/jasig/ssp/transferobject/reports/BaseStudentReportTO.java
@@ -578,6 +578,7 @@ public class BaseStudentReportTO implements Serializable {
 	
 	public void setStudentTypeName(String studentTypeName) {
 		this.studentTypeName = studentTypeName;
+		setIsIlp(ILP.equals(studentTypeName));
 	}
 	
 	public String getStudentTypeName(){
