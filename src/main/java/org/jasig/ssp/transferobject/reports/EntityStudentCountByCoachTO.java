827ce9168f1a66ab1f2ae8ef80f4e085f67b0c91
Dan McCallum <dan.mccallum@gmail.com>
2014-09-10 15:59:20 -0700

    SSP-2782 Group on owner_id instead of created_by for Plans by Owner rpt
    Also fixes a N+1 in that report by grouping on entity properties
    rather than entity associations.

--- src/main/java/org/jasig/ssp/transferobject/reports/EntityStudentCountByCoachTO.java
+++ src/main/java/org/jasig/ssp/transferobject/reports/EntityStudentCountByCoachTO.java
@@ -64,6 +64,73 @@ public class EntityStudentCountByCoachTO {
 	public void setCoach(AuditPerson coach) {
 		this.coach = coach;
 	}
-		
+
+	/**
+	 * Logical setter on top of the {@code coach} field. I.e. if that field has not yet been set and the given ID
+	 * is not {@code null}, this will set it to a new {@link AuditPerson} and assign the given ID to it. Or if that
+	 * field has already been set, the given ID will just be written through to it. No other fields on {@code coach}
+	 * are modified, even if the given ID is {@code null}.
+	 *
+	 * <p>This and the other {@code setCoach*()} methods were introduced specifically for scenarios where the underlying
+	 * persistent entity is a {@link Person} rather than an {@link AuditPerson}. The other design option in that case
+	 * was to just read the {@link Person} out of the db and pass it to a {@code setCoachAsPerson()} that would
+	 * consolidate all the work that is currently spread out over several {@code setCoach*()} methods. But the specific
+	 * queries that needed to use this mechanism were using Hibernate projections grouped by entities ({@link Person}
+	 * in this case) rather than simpler entity properties, which leads to N+1 query patterns. See
+	 * <a href="http://stackoverflow.com/questions/4330480/prevent-hibernate-n1-selects-when-grouping-by-an-entity">this SO thread</a>.
+	 * The workaround was to group by each of the fields that correspond an {@link AuditPerson}'s internals. Thus the
+	 * need for the fine-grained logical setters in this class.</p>
+	 *
+	 * <p>Since this is a logical setter and thus might not behave quite as you expect were you to invoke a corresponding
+	 * getter, and to avoid any possible accidents with JSON serialization, we've elected to not define getters for these
+	 * logical setters. That said, are fairly weak justifications, though, so if a need arises in the future, it's
+	 * probably OK to go ahead and add them.</p>
+	 *
+	 * @param id
+	 */
+	public void setCoachId(UUID id) {
+		if ( this.coach == null ) {
+			if ( id == null ) {
+				return;
+			}
+			this.coach = new AuditPerson(id);
+		} else {
+			this.coach.setId(id);
+		}
+	}
+
+	/**
+	 * See {@link #setCoachId(java.util.UUID)}
+	 *
+	 * @param firstName
+	 */
+	public void setCoachFirstName(String firstName) {
+		if ( this.coach == null ) {
+			if ( firstName == null ) {
+				return;
+			}
+			this.coach = new AuditPerson();
+			this.coach.setFirstName(firstName);
+		} else {
+			this.coach.setFirstName(firstName);
+		}
+	}
+
+	/**
+	 * See {@link #setCoachId(java.util.UUID)}.
+	 *
+	 * @param lastName
+	 */
+	public void setCoachLastName(String lastName) {
+		if ( this.coach == null ) {
+			if ( lastName == null ) {
+				return;
+			}
+			this.coach = new AuditPerson();
+			this.coach.setLastName(lastName);
+		} else {
+			this.coach.setLastName(lastName);
+		}
+	}
 	
 }
