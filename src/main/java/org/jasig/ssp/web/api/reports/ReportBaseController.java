800b40500a1fe7cd5ec2d7af8291aa52f39f8c3b
Dan McCallum <dan.mccallum@gmail.com>
2014-09-25 15:39:04 -0700

    SSP-2427 Embed PDF fonts to render certain HTML font styles
    Fixes bold and italics rendering in HTML text fields in Jasper
    PDF reports.
    
    Based on a solution proposed here:
    
      http://stackoverflow.com/a/24054577

--- src/main/java/org/jasig/ssp/web/api/reports/ReportBaseController.java
+++ src/main/java/org/jasig/ssp/web/api/reports/ReportBaseController.java
@@ -135,6 +135,9 @@ abstract class ReportBaseController<R> extends AbstractBaseController {
 		if (REPORT_TYPE_PDF.equals(reportType)) {
 			DefaultJasperReportsContext context = DefaultJasperReportsContext.getInstance();
 			JRPropertiesUtil.getInstance(context).setProperty("net.sf.jasperreports.awt.ignore.missing.font", "true");
+			JRPropertiesUtil.getInstance(context).setProperty("net.sf.jasperreports.default.font.name", "DejaVu Sans");
+			JRPropertiesUtil.getInstance(context).setProperty("net.sf.jasperreports.default.pdf.embedded", "true");
+			JRPropertiesUtil.getInstance(context).setProperty("net.sf.jasperreports.default.pdf.font.name", "DejaVu Sans");
 		}
 		
 		JasperFillManager.fillReportToStream(is, os, reportParameters, beanDS);
