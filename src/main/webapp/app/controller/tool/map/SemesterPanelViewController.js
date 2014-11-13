64e7ccd096405e0d7fe6bb0b7839814cacfdf6bb
Dan McCallum <dan.mccallum@gmail.com>
2014-11-13 11:55:17 -0700

    SSP-2969 Clean up MAP dialogs before exiting tool
    Affects three dialogs. Here they are, with other related behavior
    changes:
    
      1) Course details - Can only have one open at a time, and addressed
         a requisite loading race condition
    
      2) Course edit - Can only have one open at a time *per term*
    
      3) Term notes

--- src/main/webapp/app/controller/tool/map/SemesterPanelViewController.js
+++ src/main/webapp/app/controller/tool/map/SemesterPanelViewController.js
@@ -124,8 +124,10 @@ Ext.define('Ssp.controller.tool.map.SemesterPanelViewController', {
 	
 	onTermNotesButtonClick: function() {
 		var me = this;
-		if(me.termNotesPopUp == null || me.termNotesPopUp.isDestroyed)
-        	me.termNotesPopUp = Ext.create('Ssp.view.tools.map.TermNotes');
+		if ( me.termNotesPopUp ) {
+			me.termNotesPopUp.destroy();
+		}
+        me.termNotesPopUp = Ext.create('Ssp.view.tools.map.TermNotes');
 		
 	    var termNote = me.currentMapPlan.getTermNoteByTermCode(me.getView().itemId);
 	    me.termNotesPopUp.query('form')[0].getForm().loadRecord(termNote);
@@ -179,7 +181,10 @@ Ext.define('Ssp.controller.tool.map.SemesterPanelViewController', {
     onItemDblClick: function(grid, record, item, index, e, eOpts) {
 		var me = this;
 		var courseRecord = record;
-		
+
+			if ( me.coursePlanDetails ) {
+				me.coursePlanDetails.destroy();
+			}
     		me.coursePlanDetails = Ext.create('Ssp.view.tools.map.CourseNotes',{enableFields : me.getView().editable});
     		me.coursePlanDetails.parentGrid = me.getView();
     		
@@ -511,7 +516,13 @@ Ext.define('Ssp.controller.tool.map.SemesterPanelViewController', {
 	},
 	destroy: function(){
 		var me=this;
-		
+
+		if ( me.termNotesPopUp ) {
+			me.termNotesPopUp.destroy();
+		}
+		if ( me.coursePlanDetails ) {
+			me.coursePlanDetails.destroy();
+		}
 		me.getIsImportantTermButton().removeListener("move", me.setTermNoteButton, me);
 		me.getView().view.removeListener('beforedrop', me.onBeforeDrop, me);
 		me.getView().view.removeListener('drop', me.onDrop, me);
