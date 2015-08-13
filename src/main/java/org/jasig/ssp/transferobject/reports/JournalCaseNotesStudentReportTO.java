4f066fc03af9da96e6667d7aa3e00fc57697cd65
Stanley, James T <jstanley@unicon.net>
2015-08-13 13:53:51 -0700

    SSP-3093 removed unused hasCaseNotes, reformatted report to support windows text size

--- src/main/java/org/jasig/ssp/transferobject/reports/JournalCaseNotesStudentReportTO.java
+++ src/main/java/org/jasig/ssp/transferobject/reports/JournalCaseNotesStudentReportTO.java
@@ -22,14 +22,8 @@ public class JournalCaseNotesStudentReportTO extends BaseStudentReportTO {
 
 	private static final long serialVersionUID = 7431422387894606843L;
 	
-	Boolean hasCaseNotes;
 	Long caseNoteEntries;
-	public Boolean getHasCaseNotes() {
-		return hasCaseNotes;
-	}
-	public void setHasCaseNotes(Boolean hasCaseNotes) {
-		this.hasCaseNotes = hasCaseNotes;
-	}
+
 	public Long getCaseNoteEntries() {
 		return caseNoteEntries;
 	}
